52fd5eda01f649a72fc105467e7765c1f78f320d
smklimenko <smklimenko@localhost>
2011-01-13 17:22:20 +0300

    HibernateAccountManager is implemented HibernateLockAccountManager is implemented

--- modules/base/src/main/java/wisematches/server/player/locks/LockAccountInfo.java
+++ modules/base/src/main/java/wisematches/server/player/locks/LockAccountInfo.java
@@ -2,107 +2,22 @@ package wisematches.server.player.locks;
 
 import wisematches.server.player.Player;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
 /**
  * Information about lock of account. Who, when and why locked the player.
  *
- * @author <a href="mailto:smklimenko@gmail.com">Sergey Klimenko</a>
+ * @author Sergey Klimenko (smklimenko@gmail.com)
  */
-@Entity
-@Table(name = "user_lock")
-public class LockAccountInfo implements Serializable {
-	@Transient
-	private Player player;
+public interface LockAccountInfo extends Serializable {
+	Player getPlayer();
 
-	@Id
-	@Column(name = "playerId")
-	private long playerId;
+	String getPublicReason();
 
-	@Column(name = "publicReason")
-	private String publicReason;
+	String getPrivateReason();
 
-	@Column(name = "privateReason")
-	private String privateReason;
+	Date getLockDate();
 
-	@Column(name = "lockDate")
-	private Date lockDate;
-
-	@Column(name = "unlockDate")
-	private Date unlockDate;
-
-
-	/**
-	 * Constructor for Hibernate
-	 */
-	public LockAccountInfo() {
-	}
-
-	public LockAccountInfo(Player player, String publicReason, String privateReason, Date unlockDate) {
-		this.player = player;
-		this.playerId = player.getId();
-		this.publicReason = publicReason;
-		this.privateReason = privateReason;
-		this.lockDate = new Date();
-		this.unlockDate = unlockDate;
-	}
-
-	public Player getPlayer() {
-		return player;
-	}
-
-	public void setPlayer(Player player) {
-		this.player = player;
-		this.playerId = player.getId();
-	}
-
-	public long getPlayerId() {
-		return playerId;
-	}
-
-	public String getPublicReason() {
-		return publicReason;
-	}
-
-	public String getPrivateReason() {
-		return privateReason;
-	}
-
-	public Date getLockDate() {
-		return lockDate;
-	}
-
-	public Date getUnlockDate() {
-		return unlockDate;
-	}
-
-	public void setLockDate(Date lockDate) {
-		this.lockDate = lockDate;
-	}
-
-	public void setPublicReason(String publicReason) {
-		this.publicReason = publicReason;
-	}
-
-	public void setPrivateReason(String privateReason) {
-		this.privateReason = privateReason;
-	}
-
-	public void setUnlockDate(Date unlockDate) {
-		this.unlockDate = unlockDate;
-	}
-
-	@Override
-	public String toString() {
-		return "LockAccountInfo{" +
-				"player=" + player +
-				", playerId=" + playerId +
-				", publicReason='" + publicReason + '\'' +
-				", privateReason='" + privateReason + '\'' +
-				", lockDate=" + lockDate +
-				", unlockDate=" + unlockDate +
-				'}';
-	}
+	Date getUnlockDate();
 }
--- modules/base/src/test/java/wisematches/server/player/locks/impl/HibernateLockAccountManagerTest.java
+++ modules/base/src/test/java/wisematches/server/player/locks/impl/HibernateLockAccountManagerTest.java
@@ -1,39 +1,62 @@
 package wisematches.server.player.locks.impl;
 
-import org.springframework.jdbc.support.rowset.SqlRowSet;
-import org.springframework.test.AbstractTransactionalDataSourceSpringContextTests;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.AfterTransaction;
+import org.springframework.test.context.transaction.BeforeTransaction;
+import org.springframework.transaction.annotation.Transactional;
+import wisematches.server.player.AccountManager;
 import wisematches.server.player.Player;
+import wisematches.server.player.PlayerEditor;
 import wisematches.server.player.locks.LockAccountInfo;
 import wisematches.server.player.locks.LockAccountListener;
 import wisematches.server.player.locks.LockAccountManager;
 import wisematches.server.player.locks.LockUsernameListener;
 
-import java.sql.Types;
 import java.util.Date;
 
 import static org.easymock.EasyMock.*;
+import static org.junit.Assert.*;
 
 /**
  * @author <a href="mailto:smklimenko@gmail.com">Sergey Klimenko</a>
  */
-public class HibernateLockAccountManagerTest extends AbstractTransactionalDataSourceSpringContextTests {
-	private LockAccountListener accountListener;
-	private LockUsernameListener usernameListener;
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+		"classpath:/config/test-server-base-config.xml",
+		"classpath:/config/database-config.xml",
+		"classpath:/config/server-base-config.xml"
+})
+public class HibernateLockAccountManagerTest {
+	@Autowired
 	private LockAccountManager lockAccountManager;
+
+	@Autowired
+	private AccountManager accountManager;
+
 	private Player player;
 
-	protected String[] getConfigLocations() {
-		return new String[]{"classpath:/config/test-server-base-config.xml"};
+	private LockAccountListener accountListener;
+	private LockUsernameListener usernameListener;
+
+	@Before
+	public void createPlayer() throws Exception {
+		player = accountManager.createPlayer(new PlayerEditor("asd@qwe.ru", "qwe", "asd").createPlayer());
 	}
 
-	@Override
-	protected void onSetUp() throws Exception {
-		super.onSetUp();
+	@After
+	public void removePlayer() throws Exception {
+		accountManager.removePlayer(player);
+	}
 
-		player = createMock(Player.class);
-		expect(player.getId()).andReturn(13L).anyTimes();
-		replay(player);
-
+	@BeforeTransaction
+	public void onSetUp() throws Exception {
 		accountListener = createStrictMock(LockAccountListener.class);
 		usernameListener = createStrictMock(LockUsernameListener.class);
 
@@ -41,149 +64,78 @@ public class HibernateLockAccountManagerTest extends AbstractTransactionalDataSo
 		lockAccountManager.addLockUsernameListener(usernameListener);
 	}
 
-
-	@Override
-	protected void onTearDown() throws Exception {
-		jdbcTemplate.update("delete from lock_account where playerId = ?", new Object[]{player.getId()});
-		jdbcTemplate.update("delete from lock_username where username = ?", new Object[]{"test"});
-
+	@AfterTransaction
+	public void onTearDown() throws Exception {
 		lockAccountManager.removeLockAccountListener(accountListener);
 		lockAccountManager.removeLockUsernameListener(usernameListener);
-
-		super.onTearDown();
 	}
 
-	public void test_lockUnlock() {
-		SqlRowSet rowSet;
-
-		final int count = countRowsInTable("lock_account");
-
-		final Date createDate1 = new Date((System.currentTimeMillis() / 1000) * 1000); //remove milliseconds
-		final Date unlockDate1 = new Date(createDate1.getTime() + 10000);
+	@Test
+	public void testLockUnlockAccount() {
+		final Date unlockDate = new Date(new Date().getTime() + 10000);
 
 		reset(accountListener);
-		accountListener.accountLocked(player, "You are locked", "Fuck off", unlockDate1);
+		accountListener.accountLocked(player, "You are locked", "Fuck off", unlockDate);
 		replay(accountListener);
 
-		lockAccountManager.lockAccount(player, "You are locked", "Fuck off", unlockDate1);
-		assertEquals(count + 1, countRowsInTable("lock_account"));
-		verify(accountListener);
-
-		rowSet = jdbcTemplate.queryForRowSet("select * from lock_account where playerId = " + player.getId());
-		assertTrue(rowSet.next());
-
-		assertEquals(13L, rowSet.getLong(1));
-		assertEquals("You are locked", rowSet.getString(2));
-		assertEquals("Fuck off", rowSet.getString(3));
-		//creation time must be not great that 2 seconds
-		assertTrue(Math.abs(createDate1.getTime() - rowSet.getTimestamp(4).getTime()) < 2000);
-
-		//double lock must update parameters
-		final Date createDate2 = new Date((System.currentTimeMillis() / 1000) * 1000); //remove milliseconds
-		final Date unlockDate2 = new Date(createDate2.getTime() + 20000);
-		reset(accountListener);
-		accountListener.accountLocked(player, "You are locked 2", "Fuck off 2", unlockDate2);
-		replay(accountListener);
-
-		lockAccountManager.lockAccount(player, "You are locked 2", "Fuck off 2", unlockDate2);
-		assertEquals(count + 1, countRowsInTable("lock_account"));
-		verify(accountListener);
-
-		rowSet = jdbcTemplate.queryForRowSet("select * from lock_account where playerId = " + player.getId());
-		rowSet.next();
-		assertEquals(13L, rowSet.getLong(1));
-		assertEquals("You are locked 2", rowSet.getString(2));
-		assertEquals("Fuck off 2", rowSet.getString(3));
-		//creation time must be not great that 2 seconds
-		assertTrue(Math.abs(createDate2.getTime() - rowSet.getTimestamp(4).getTime()) < 2000);
-		assertEquals(unlockDate2, new Date(rowSet.getTimestamp(5).getTime()));
-
-		reset(accountListener);
-		accountListener.accountUnlocked(player);
-		replay(accountListener);
-
-		lockAccountManager.unlockAccount(player);
-		assertEquals(count, countRowsInTable("lock_account"));
-
-		//Double unlock dosn't have affect
-		lockAccountManager.unlockAccount(player);
-		assertEquals(count, countRowsInTable("lock_account"));
-		verify(accountListener);
-	}
-
-	public void test_isAccountLocked() throws InterruptedException {
-		final Date date = new Date(System.currentTimeMillis() + 2000);
-
-		final int count = countRowsInTable("lock_account");
-
-		accountListener.accountLocked(player, "t", "t", date);
-		accountListener.accountUnlocked(player);
-		replay(accountListener);
-
-		lockAccountManager.lockAccount(player, "t", "t", date);
+		assertFalse(lockAccountManager.isAccountLocked(player));
+		lockAccountManager.lockAccount(player, "You are locked", "Fuck off", unlockDate);
 		assertTrue(lockAccountManager.isAccountLocked(player));
-		assertEquals(count + 1, countRowsInTable("lock_account"));
+		verify(accountListener);
+
+		reset(accountListener);
+		accountListener.accountUnlocked(player);
+		replay(accountListener);
+
+		lockAccountManager.unlockAccount(player);
+		assertFalse(lockAccountManager.isAccountLocked(player));
+		verify(accountListener);
+	}
+
+	@Test
+	public void testIsAccountLocked() throws InterruptedException {
+		final Date unlockDate = new Date(System.currentTimeMillis() + 1000);
+
+		assertFalse(lockAccountManager.isAccountLocked(new PlayerEditor("asd", "qwe", "zc").createPlayer()));
+
+		reset(accountListener);
+		accountListener.accountLocked(player, "t", "t", unlockDate);
+		accountListener.accountUnlocked(player);
+		replay(accountListener);
+
+		lockAccountManager.lockAccount(player, "t", "t", unlockDate);
+		assertTrue(lockAccountManager.isAccountLocked(player));
 
 		//Now wait while lock timeout expired
-		Thread.sleep(2200);
+		Thread.sleep(1200);
 		assertFalse(lockAccountManager.isAccountLocked(player));
-		assertEquals(count, countRowsInTable("lock_account"));
-
-		assertFalse(lockAccountManager.isAccountLocked(player));
-		assertEquals(count, countRowsInTable("lock_account"));
-		verify(accountListener);
 	}
 
-	public void test_getLockAccountInfo() throws InterruptedException {
-		final int count = countRowsInTable("lock_account");
-		System.err.println("Count: " + count);
-
+	@Test
+	public void testGetLockAccountInfo() throws InterruptedException {
 		final Date lockDate = new Date((System.currentTimeMillis() / 1000) * 1000);
-		final Date unlockDate = new Date(lockDate.getTime() + 2000);
-		System.err.println("Lock date: " + lockDate.getTime() + ", unlockDate - " + unlockDate.getTime());
+		final Date unlockDate = new Date(lockDate.getTime() + 1000);
 
-		int k = jdbcTemplate.update(
-				"insert into lock_account(playerId, publicReason, privateReason, lockDate, unlockDate) " +
-						"values (?, ?, ?, ?, ?)",
-				new Object[]{player.getId(), "T1", "T2", lockDate, unlockDate},
-				new int[]{Types.TINYINT, Types.VARCHAR, Types.VARCHAR, Types.TIMESTAMP, Types.TIMESTAMP});
-		System.err.println("Record inserted: " + k);
-		assertEquals("Record inserted", 1, k);
-
-		final SqlRowSet rowSet = jdbcTemplate.queryForRowSet("select playerId, publicReason, privateReason, lockDate, unlockDate from lock_account");
-		while (rowSet.next()) {
-			final long aLong = rowSet.getLong(1);
-			final String s1 = rowSet.getString(2);
-			final String s2 = rowSet.getString(3);
-			Date d1 = rowSet.getTimestamp(4);
-			Date d2 = rowSet.getTimestamp(5);
-			System.err.println("Player locked: " + aLong + ", " + s1 + ", " + s2 + ", " + d1.getTime() + ", " + d2.getTime());
-		}
-
-		final LockAccountInfo accountInfo = lockAccountManager.getLockAccountInfo(player);
-		System.err.println("Lock info: " + accountInfo);
-		assertNotNull(accountInfo);
-		assertSame(player, accountInfo.getPlayer());
-		assertEquals(13, accountInfo.getPlayerId());
-		assertEquals("T1", accountInfo.getPublicReason());
-		assertEquals("T2", accountInfo.getPrivateReason());
-		//creation time must be not great that 2 seconds
-		assertTrue(Math.abs(lockDate.getTime() - accountInfo.getLockDate().getTime()) < 2000);
-		assertEquals(unlockDate.getTime() / 1000 * 1000, accountInfo.getUnlockDate().getTime());
-		assertEquals(count + 1, jdbcTemplate.queryForInt("select count(*) from lock_account"));
-
+		reset(accountListener);
+		accountListener.accountLocked(player, "t1", "t2", unlockDate);
 		accountListener.accountUnlocked(player);
 		replay(accountListener);
 
-		Thread.sleep(2200);
-		assertNull(lockAccountManager.getLockAccountInfo(player));
-		assertEquals(count, countRowsInTable("lock_account"));
+		lockAccountManager.lockAccount(player, "t1", "t2", unlockDate);
+		assertNull(lockAccountManager.getLockAccountInfo(new PlayerEditor("asd", "qwe", "zc").createPlayer()));
+		final LockAccountInfo info = lockAccountManager.getLockAccountInfo(player);
+		assertEquals(player, info.getPlayer());
+		assertEquals("t1", info.getPublicReason());
+		assertEquals("t2", info.getPrivateReason());
+		assertTrue(Math.abs(lockDate.getTime() - info.getLockDate().getTime()) < 2000);
+		assertEquals(unlockDate, info.getUnlockDate());
 
+		//Now wait while lock timeout expired
+		Thread.sleep(1200);
 		assertNull(lockAccountManager.getLockAccountInfo(player));
-		assertEquals(count, countRowsInTable("lock_account"));
-		verify(accountListener);
 	}
 
+	@Test
 	public void test_username() {
 		usernameListener.usernameLocked("test", "test reason");
 		usernameListener.usernameLocked("test", "test reason2");
@@ -192,33 +144,23 @@ public class HibernateLockAccountManagerTest extends AbstractTransactionalDataSo
 
 		lockAccountManager.addLockUsernameListener(usernameListener);
 
-		final int count = countRowsInTable("lock_username");
 		assertNull(lockAccountManager.isUsernameLocked("test"));
 
 		lockAccountManager.lockUsername("test", "test reason");
 		assertEquals("test reason", lockAccountManager.isUsernameLocked("test"));
-		assertEquals(count + 1, countRowsInTable("lock_username"));
 
 		lockAccountManager.lockUsername("test", "test reason2");
 		assertEquals("test reason2", lockAccountManager.isUsernameLocked("test"));
-		assertEquals(count + 1, countRowsInTable("lock_username"));
 
 		lockAccountManager.unlockUsername("test2");
-		assertEquals(count + 1, countRowsInTable("lock_username"));
 		assertEquals("test reason2", lockAccountManager.isUsernameLocked("test"));
 
 		lockAccountManager.unlockUsername("test");
 		assertNotNull(lockAccountManager.isUsernameLocked("test"));
-		assertEquals(count, countRowsInTable("lock_username"));
 
 		lockAccountManager.unlockUsername("test");
 		assertNotNull(lockAccountManager.isUsernameLocked("test"));
-		assertEquals(count, countRowsInTable("lock_username"));
 
 		verify(usernameListener);
-	}
-
-	public void setLockAccountManager(LockAccountManager lockAccountManager) {
-		this.lockAccountManager = lockAccountManager;
 	}
 }
