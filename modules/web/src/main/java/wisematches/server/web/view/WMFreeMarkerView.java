d7abce39d2c7bb3e7904cba95f9261fc8ce23ddf
smklimenko <smklimenko@localhost>
2011-04-07 18:21:13 +0400

    Issue 4:	No exception processing page

--- modules/web/src/main/java/wisematches/server/web/view/WMFreeMarkerView.java
+++ modules/web/src/main/java/wisematches/server/web/view/WMFreeMarkerView.java
@@ -4,6 +4,7 @@ import freemarker.ext.servlet.FreemarkerServlet;
 import freemarker.ext.servlet.HttpRequestParametersHashModel;
 import freemarker.template.SimpleHash;
 import freemarker.template.Template;
+import freemarker.template.TemplateBooleanModel;
 import freemarker.template.TemplateException;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
 
@@ -20,7 +21,8 @@ public class WMFreeMarkerView extends FreeMarkerView {
 	@Override
 	protected void processTemplate(Template template, SimpleHash model, HttpServletResponse response) throws IOException, TemplateException {
 		final HttpRequestParametersHashModel sm2 = (HttpRequestParametersHashModel) model.get(FreemarkerServlet.KEY_REQUEST_PARAMETERS);
-		if ("true".equalsIgnoreCase(String.valueOf(sm2.get("plain")))) {
+		if ("true".equalsIgnoreCase(String.valueOf(sm2.get("plain"))) ||
+				(model.get("plain") != null && ((TemplateBooleanModel) model.get("plain")).getAsBoolean())) {
 			super.processTemplate(template, model, response);
 		} else {
 			model.put("originalTemplateName", getUrl());
