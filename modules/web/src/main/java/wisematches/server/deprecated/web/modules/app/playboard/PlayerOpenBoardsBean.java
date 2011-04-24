fc51ca7efae8fc1f88cf4791d5032f3ac29828f7
smklimenko <smklimenko@localhost>
2011-04-24 09:10:51 +0400

    Deprecated packages removed

--- modules/web/src/main/java/wisematches/server/deprecated/web/modules/app/playboard/PlayerOpenBoardsBean.java
+++
@@ -1,9 +0,0 @@
-package wisematches.server.deprecated.web.modules.app.playboard;
-
-import wisematches.server.utils.sessions.PlayerSessionBean;
-
-/**
- * @author <a href="mailto:smklimenko@gmail.com">Sergey Klimenko</a>
- */
-public interface PlayerOpenBoardsBean extends PlayerSessionBean, OpenBoardsBean {
-}
