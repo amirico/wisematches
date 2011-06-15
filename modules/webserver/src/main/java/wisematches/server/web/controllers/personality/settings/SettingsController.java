bf9ffd73a6195b003d0b5dfbeadc7e1052cbf071
smklimenko <smklimenko@localhost>
2011-06-16 01:06:26 +0400

    + Issue 14: 	No settings page

--- modules/webserver/src/main/java/wisematches/server/web/controllers/personality/settings/SettingsController.java
+++ modules/webserver/src/main/java/wisematches/server/web/controllers/personality/settings/SettingsController.java
@@ -1,7 +1,8 @@
 package wisematches.server.web.controllers.personality.settings;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,19 +11,17 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.support.SessionStatus;
 import wisematches.personality.Language;
 import wisematches.personality.account.*;
 import wisematches.personality.player.Player;
-import wisematches.server.mail.MailService;
-import wisematches.server.security.PlayerSecurityService;
+import wisematches.server.security.AccountSecurityService;
 import wisematches.server.web.controllers.AbstractPlayerController;
+import wisematches.server.web.controllers.UnknownEntityException;
 import wisematches.server.web.controllers.personality.settings.form.SettingsForm;
-import wisematches.server.web.controllers.personality.settings.form.TimeZonesInfo;
+import wisematches.server.web.controllers.personality.settings.form.TimeZoneInfo;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.Set;
 import java.util.TimeZone;
 
 /**
@@ -31,123 +30,134 @@ import java.util.TimeZone;
 @Controller
 @RequestMapping("/account/modify")
 public class SettingsController extends AbstractPlayerController {
-	private MailService mailService;
-	private AccountManager accountManager;
-	private PlayerSecurityService playerSecurityService;
+    private AccountManager accountManager;
+    private AccountSecurityService accountSecurityService;
 
-	public SettingsController() {
-	}
+    private static final Log log = LogFactory.getLog("wisematches.server.web.settings");
 
-	@RequestMapping(value = "", method = RequestMethod.GET)
-	public String modifyAccountPage(Model model,
-									@ModelAttribute("settings")
-									SettingsForm form) {
-		final Player principal = getPrincipal();
-		if (principal.getTimeZone() != null) {
-			form.setTimezone(principal.getTimeZone().getDisplayName());
-		}
-		form.setLanguage(principal.getLanguage().name().toLowerCase());
-		form.setEmail(principal.getEmail());
-		model.addAttribute("timeZones", TimeZonesInfo.getTimeZones());
-		return "/content/personality/settings/template";
-	}
+    public SettingsController() {
+    }
 
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	@RequestMapping(value = "", method = RequestMethod.POST)
-	public String modifyAccountAction(HttpServletRequest request, HttpServletResponse response,
-									  @Valid @ModelAttribute("settings") SettingsForm form,
-									  BindingResult errors, Model model, SessionStatus status) {
-		final Account account = accountManager.getAccount(getPersonality().getId());
-		if (account == null) {
-			errors.reject("unknown.player");
-		}
+    @RequestMapping(value = "")
+    public String modifyAccountPage(Model model, @ModelAttribute("settings") SettingsForm form) {
+        final Player principal = getPrincipal();
+        if (principal.getTimeZone() != null) {
+            form.setTimezone(principal.getTimeZone().getID());
+        }
+        form.setLanguage(principal.getLanguage().name().toLowerCase());
+        form.setEmail(principal.getEmail());
+        model.addAttribute("timeZones", TimeZoneInfo.getTimeZones());
+        return "/content/personality/settings/template";
+    }
 
-		Language language = account.getLanguage();
-		if (form.getLanguage() != null) {
-			try {
-				language = Language.valueOf(form.getLanguage().toUpperCase());
-			} catch (IllegalArgumentException ex) {
-				errors.rejectValue("language", "unknown.language");
-			}
-		}
+    @RequestMapping(value = "done")
+    public String modifyAccountDone(Model model, @ModelAttribute("settings") SettingsForm form) {
+        model.addAttribute("saved", Boolean.TRUE);
+        return modifyAccountPage(model, form);
+    }
 
-		TimeZone timeZone = account.getTimeZone();
-		if (form.getTimezone() != null) {
-			timeZone = TimeZone.getTimeZone(form.getTimezone());
-			if (timeZone == null) {
-				errors.rejectValue("timezone", "unknown.timezone");
-			}
-		}
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String modifyAccountAction(@Valid @ModelAttribute("settings") SettingsForm form,
+                                      BindingResult errors, Model model) throws UnknownEntityException {
+        final Account account = accountManager.getAccount(getPersonality().getId());
+        if (account == null) {
+            throw new UnknownEntityException(null, "account");
+        }
 
-		if (form.isChangeEmail() && form.getEmail().trim().isEmpty()) {
-			errors.rejectValue("email", "account.register.email.err.blank");
-		}
+        Language language = account.getLanguage();
+        if (form.getLanguage() != null) {
+            try {
+                language = Language.valueOf(form.getLanguage().toUpperCase());
+            } catch (IllegalArgumentException ex) {
+                errors.rejectValue("language", "account.register.language.err.unknown");
+            }
+        }
 
-		if (form.isChangePassword()) {
-			if (form.getPassword().trim().isEmpty()) {
-				errors.rejectValue("password", "account.register.pwd.err.blank");
-			}
-			if (form.getConfirm().trim().isEmpty()) {
-				errors.rejectValue("confirm", "account.register.pwd-cfr.err.blank");
-			}
+        TimeZone timeZone = account.getTimeZone();
+        if (form.getTimezone() != null) {
+            timeZone = TimeZone.getTimeZone(form.getTimezone());
+            if (timeZone == null) {
+                errors.rejectValue("timezone", "account.register.timezone.err.unknown");
+            }
+        }
 
-			if (!form.getPassword().equals(form.getConfirm())) {
-				errors.rejectValue("confirm", "account.register.pwd-cfr.err.mismatch");
-			}
-		}
+        if (form.isChangeEmail() && form.getEmail().trim().isEmpty()) {
+            errors.rejectValue("email", "account.register.email.err.blank");
+        }
 
-		if (!errors.hasErrors()) {
-			final AccountEditor editor = new AccountEditor(account);
-			if (language != editor.getLanguage()) {
-				editor.setLanguage(language);
-			}
-			if (!editor.getTimeZone().equals(timeZone)) {
-				editor.setTimeZone(timeZone);
-			}
+        if (form.isChangePassword()) {
+            if (form.getPassword().trim().isEmpty()) {
+                errors.rejectValue("password", "account.register.pwd.err.blank");
+            }
+            if (form.getConfirm().trim().isEmpty()) {
+                errors.rejectValue("confirm", "account.register.pwd-cfr.err.blank");
+            }
 
-			if (form.isChangeEmail() && !editor.getEmail().equals(form.getEmail())) {
-				editor.setEmail(form.getEmail());
-			}
+            if (!form.getPassword().equals(form.getConfirm())) {
+                errors.rejectValue("confirm", "account.register.pwd-cfr.err.mismatch");
+            }
+        }
 
-			if (form.isChangePassword()) {
-				if (playerSecurityService != null) {
-					editor.setPassword(playerSecurityService.encodePlayerPassword(editor.createAccount(), form.getPassword()));
-				} else {
-					editor.setPassword(form.getPassword());
-				}
-			}
+        if (!errors.hasErrors()) {
+            final AccountEditor editor = new AccountEditor(account);
+            if (language != editor.getLanguage()) {
+                editor.setLanguage(language);
+            }
+            if (!editor.getTimeZone().equals(timeZone)) {
+                editor.setTimeZone(timeZone);
+            }
 
-			try {
-				accountManager.updateAccount(editor.createAccount());
-			} catch (UnknownAccountException e) {
-				errors.reject("unknown.error");
-			} catch (DuplicateAccountException e) {
-				errors.reject("unknown.error");
-			} catch (InadmissibleUsernameException e) {
-				errors.reject("unknown.error");
-			}
-		}
+            if (form.isChangeEmail() && !editor.getEmail().equals(form.getEmail())) {
+                editor.setEmail(form.getEmail());
+            }
 
-		return modifyAccountPage(model, form);
-	}
+            if (form.isChangePassword()) {
+                if (accountSecurityService != null) {
+                    editor.setPassword(accountSecurityService.encodePlayerPassword(editor.createAccount(), form.getPassword()));
+                } else {
+                    editor.setPassword(form.getPassword());
+                }
+            }
 
-	@ModelAttribute("headerTitle")
-	public String getHeaderTitle() {
-		return "title.settings";
-	}
+            try {
+                final Account a = accountManager.updateAccount(editor.createAccount());
+                if (accountSecurityService != null) {
+                    accountSecurityService.authenticatePlayer(a, form.isChangePassword() ? form.getPassword() : null);
+                }
+                return "redirect:/account/modify/done";
+            } catch (UnknownAccountException e) {
+                throw new UnknownEntityException(null, "account");
+            } catch (DuplicateAccountException ex) {
+                final Set<String> fieldNames = ex.getFieldNames();
+                if (fieldNames.contains("email")) {
+                    errors.rejectValue("email", "account.register.form.email.err.busy");
+                }
+                if (fieldNames.contains("nickname")) {
+                    errors.rejectValue("nickname", "account.register.form.username.err.incorrect");
+                }
+            } catch (InadmissibleUsernameException ex) {
+                errors.rejectValue("username", "account.register.form.username.err.incorrect");
+            } catch (Exception ex) {
+                log.error("Account can't be created", ex);
+                errors.reject("wisematches.error.internal");
+            }
+        }
+        return modifyAccountPage(model, form);
+    }
 
-	@Autowired
-	public void setMailService(@Qualifier("mailService") MailService mailService) {
-		this.mailService = mailService;
-	}
+    @ModelAttribute("headerTitle")
+    public String getHeaderTitle() {
+        return "title.settings";
+    }
 
-	@Autowired
-	public void setAccountManager(AccountManager accountManager) {
-		this.accountManager = accountManager;
-	}
+    @Autowired
+    public void setAccountManager(AccountManager accountManager) {
+        this.accountManager = accountManager;
+    }
 
-	@Autowired
-	public void setPlayerSecurityService(PlayerSecurityService playerSecurityService) {
-		this.playerSecurityService = playerSecurityService;
-	}
+    @Autowired
+    public void setAccountSecurityService(AccountSecurityService accountSecurityService) {
+        this.accountSecurityService = accountSecurityService;
+    }
 }
--- modules/webserver/src/main/webapp/content/personality/settings/template.ftl
+++ modules/webserver/src/main/webapp/content/personality/settings/template.ftl
@@ -1,18 +1,18 @@
 <#include "/core.ftl">
 
-<form action="modify" method="post">
+<form action="modify/save" method="post">
     <div class="profile">
         <div id="settings" class="shadow">
             <ul class="settings-tabs">
-                <li><a href="#commonTab">Common</a></li>
-                <li><a href="#notificationsTab">Notifications</a></li>
+                <li><a href="#commonTab"><@message code="account.modify.common.label"/></a></li>
+            <#--<li><a href="#notificationsTab">Notifications</a></li>-->
             </ul>
             <div id="commonTab" class="ui-state-default">
             <#include "common.ftl"/>
             </div>
-            <div id="notificationsTab" class="ui-state-default">
-            <#include "notifications.ftl"/>
-            </div>
+        <#--<div id="notificationsTab" class="ui-state-default">-->
+        <#--<#include "notifications.ftl"/>-->
+        <#--</div>-->
         </div>
     </div>
 </form>
