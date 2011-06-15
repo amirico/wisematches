ad8d1ff748b0e88ea641b2b917d4f376b2068640
smklimenko <smklimenko@localhost>
2011-06-15 19:32:17 +0400

    ~ Issue 14:	No settings page

--- modules/webserver/src/main/java/wisematches/server/web/controllers/personality/settings/SettingsController.java
+++ modules/webserver/src/main/java/wisematches/server/web/controllers/personality/settings/SettingsController.java
@@ -1,6 +1,7 @@
 package wisematches.server.web.controllers.personality.settings;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.support.SessionStatus;
 import wisematches.personality.Language;
 import wisematches.personality.account.*;
 import wisematches.personality.player.Player;
+import wisematches.server.mail.MailService;
+import wisematches.server.security.PlayerSecurityService;
 import wisematches.server.web.controllers.AbstractPlayerController;
 import wisematches.server.web.controllers.personality.settings.form.SettingsForm;
 import wisematches.server.web.controllers.personality.settings.form.TimeZonesInfo;
@@ -28,81 +31,123 @@ import java.util.TimeZone;
 @Controller
 @RequestMapping("/account/modify")
 public class SettingsController extends AbstractPlayerController {
-    private AccountManager accountManager;
+	private MailService mailService;
+	private AccountManager accountManager;
+	private PlayerSecurityService playerSecurityService;
 
-    public SettingsController() {
-    }
+	public SettingsController() {
+	}
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String modifyAccountPage(Model model,
-                                    @ModelAttribute("settings")
-                                    SettingsForm form) {
-        final Player principal = getPrincipal();
-        if (principal.getTimeZone() != null) {
-            form.setTimezone(principal.getTimeZone().getDisplayName());
-        }
-        form.setLanguage(principal.getLanguage().name().toLowerCase());
-        model.addAttribute("timeZones", TimeZonesInfo.getTimeZones());
-        return "/content/personality/settings/template";
-    }
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String modifyAccountPage(Model model,
+									@ModelAttribute("settings")
+									SettingsForm form) {
+		final Player principal = getPrincipal();
+		if (principal.getTimeZone() != null) {
+			form.setTimezone(principal.getTimeZone().getDisplayName());
+		}
+		form.setLanguage(principal.getLanguage().name().toLowerCase());
+		form.setEmail(principal.getEmail());
+		model.addAttribute("timeZones", TimeZonesInfo.getTimeZones());
+		return "/content/personality/settings/template";
+	}
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    @RequestMapping(value = "", method = RequestMethod.POST)
-    public String modifyAccountAction(HttpServletRequest request, HttpServletResponse response,
-                                      @Valid @ModelAttribute("settings") SettingsForm form,
-                                      BindingResult result, Model model, SessionStatus status) {
-        final Account account = accountManager.getAccount(getPersonality().getId());
-        if (account == null) {
-            result.reject("unknown.player");
-        }
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public String modifyAccountAction(HttpServletRequest request, HttpServletResponse response,
+									  @Valid @ModelAttribute("settings") SettingsForm form,
+									  BindingResult errors, Model model, SessionStatus status) {
+		final Account account = accountManager.getAccount(getPersonality().getId());
+		if (account == null) {
+			errors.reject("unknown.player");
+		}
 
-        Language language = account.getLanguage();
-        if (form.getLanguage() != null) {
-            try {
-                language = Language.valueOf(form.getLanguage().toUpperCase());
-            } catch (IllegalArgumentException ex) {
-                result.rejectValue("language", "unknown.language");
-            }
-        }
+		Language language = account.getLanguage();
+		if (form.getLanguage() != null) {
+			try {
+				language = Language.valueOf(form.getLanguage().toUpperCase());
+			} catch (IllegalArgumentException ex) {
+				errors.rejectValue("language", "unknown.language");
+			}
+		}
 
-        TimeZone timeZone = account.getTimeZone();
-        if (form.getTimezone() != null) {
-            timeZone = TimeZone.getTimeZone(form.getTimezone());
-            if (timeZone == null) {
-                result.rejectValue("timezone", "unknown.timezone");
-            }
-        }
+		TimeZone timeZone = account.getTimeZone();
+		if (form.getTimezone() != null) {
+			timeZone = TimeZone.getTimeZone(form.getTimezone());
+			if (timeZone == null) {
+				errors.rejectValue("timezone", "unknown.timezone");
+			}
+		}
 
-        if (!result.hasErrors()) {
-            final AccountEditor editor = new AccountEditor(account);
-            if (language != editor.getLanguage()) {
-                editor.setLanguage(language); // TODO: change language in interceptor
-            }
-            if (!editor.getTimeZone().equals(timeZone)) {
-                editor.setTimeZone(timeZone);
-            }
+		if (form.isChangeEmail() && form.getEmail().trim().isEmpty()) {
+			errors.rejectValue("email", "account.register.email.err.blank");
+		}
 
-            try {
-                accountManager.updateAccount(editor.createAccount());
-            } catch (UnknownAccountException e) {
-                result.reject("unknown.error");
-            } catch (DuplicateAccountException e) {
-                result.reject("unknown.error");
-            } catch (InadmissibleUsernameException e) {
-                result.reject("unknown.error");
-            }
-        }
+		if (form.isChangePassword()) {
+			if (form.getPassword().trim().isEmpty()) {
+				errors.rejectValue("password", "account.register.pwd.err.blank");
+			}
+			if (form.getConfirm().trim().isEmpty()) {
+				errors.rejectValue("confirm", "account.register.pwd-cfr.err.blank");
+			}
 
-        return modifyAccountPage(model, form);
-    }
+			if (!form.getPassword().equals(form.getConfirm())) {
+				errors.rejectValue("confirm", "account.register.pwd-cfr.err.mismatch");
+			}
+		}
 
-    @ModelAttribute("headerTitle")
-    public String getHeaderTitle() {
-        return "title.settings";
-    }
+		if (!errors.hasErrors()) {
+			final AccountEditor editor = new AccountEditor(account);
+			if (language != editor.getLanguage()) {
+				editor.setLanguage(language);
+			}
+			if (!editor.getTimeZone().equals(timeZone)) {
+				editor.setTimeZone(timeZone);
+			}
 
-    @Autowired
-    public void setAccountManager(AccountManager accountManager) {
-        this.accountManager = accountManager;
-    }
+			if (form.isChangeEmail() && !editor.getEmail().equals(form.getEmail())) {
+				editor.setEmail(form.getEmail());
+			}
+
+			if (form.isChangePassword()) {
+				if (playerSecurityService != null) {
+					editor.setPassword(playerSecurityService.encodePlayerPassword(editor.createAccount(), form.getPassword()));
+				} else {
+					editor.setPassword(form.getPassword());
+				}
+			}
+
+			try {
+				accountManager.updateAccount(editor.createAccount());
+			} catch (UnknownAccountException e) {
+				errors.reject("unknown.error");
+			} catch (DuplicateAccountException e) {
+				errors.reject("unknown.error");
+			} catch (InadmissibleUsernameException e) {
+				errors.reject("unknown.error");
+			}
+		}
+
+		return modifyAccountPage(model, form);
+	}
+
+	@ModelAttribute("headerTitle")
+	public String getHeaderTitle() {
+		return "title.settings";
+	}
+
+	@Autowired
+	public void setMailService(@Qualifier("mailService") MailService mailService) {
+		this.mailService = mailService;
+	}
+
+	@Autowired
+	public void setAccountManager(AccountManager accountManager) {
+		this.accountManager = accountManager;
+	}
+
+	@Autowired
+	public void setPlayerSecurityService(PlayerSecurityService playerSecurityService) {
+		this.playerSecurityService = playerSecurityService;
+	}
 }
--- modules/webserver/src/main/java/wisematches/server/web/controllers/personality/settings/form/SettingsForm.java
+++ modules/webserver/src/main/java/wisematches/server/web/controllers/personality/settings/form/SettingsForm.java
@@ -1,33 +1,88 @@
 package wisematches.server.web.controllers.personality.settings.form;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * @author Sergey Klimenko (smklimenko@gmail.com)
  */
 public class SettingsForm {
-    @NotEmpty(message = "account.register.language.err.blank")
-    private String language;
+	@NotEmpty(message = "account.register.language.err.blank")
+	private String language;
 
-    @NotEmpty(message = "account.register.timezone.err.blank")
-    private String timezone;
+	@NotEmpty(message = "account.register.timezone.err.blank")
+	private String timezone;
 
-    public SettingsForm() {
-    }
+	@Length(max = 150, message = "account.register.email.err.max")
+	@Email(message = "account.register.email.err.format")
+	private String email;
 
-    public String getLanguage() {
-        return language;
-    }
+	@Length(max = 100, message = "account.register.pwd.err.max")
+	private String password;
 
-    public void setLanguage(String language) {
-        this.language = language;
-    }
+	@Length(max = 100, message = "account.register.pwd-cfr.err.max")
+	private String confirm;
 
-    public String getTimezone() {
-        return timezone;
-    }
+	private boolean changeEmail = false;
+	private boolean changePassword = false;
 
-    public void setTimezone(String timezone) {
-        this.timezone = timezone;
-    }
+	public SettingsForm() {
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getTimezone() {
+		return timezone;
+	}
+
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirm() {
+		return confirm;
+	}
+
+	public void setConfirm(String confirm) {
+		this.confirm = confirm;
+	}
+
+	public boolean isChangeEmail() {
+		return changeEmail;
+	}
+
+	public void setChangeEmail(boolean changeEmail) {
+		this.changeEmail = changeEmail;
+	}
+
+	public boolean isChangePassword() {
+		return changePassword;
+	}
+
+	public void setChangePassword(boolean changePassword) {
+		this.changePassword = changePassword;
+	}
 }
