87c92a377cdd69b92b2bab155be6a54eca1f99ee
smklimenko <smklimenko@localhost>
2011-06-21 18:54:59 +0400

    notifications

--- modules/webserver/src/main/java/wisematches/server/web/services/notice/impl/HibernateNotificationManager.java
+++ modules/webserver/src/main/java/wisematches/server/web/services/notice/impl/HibernateNotificationManager.java
@@ -19,49 +19,58 @@ import java.util.Map;
  * @author Sergey Klimenko (smklimenko@gmail.com)
  */
 public class HibernateNotificationManager extends HibernateDaoSupport implements NotificationManager {
-    private final Map<String, NotificationDescription> descriptionMap = new HashMap<String, NotificationDescription>();
+	private final Map<Personality, NotificationMask> maskMap = new HashMap<Personality, NotificationMask>();
+	private final Map<String, NotificationDescription> descriptionMap = new HashMap<String, NotificationDescription>();
 
-    public HibernateNotificationManager() {
-    }
+	public HibernateNotificationManager() {
+	}
 
-    @Override
-    public boolean isNotificationEnabled(final String name, final Personality personality) {
-        getHibernateTemplate().execute(new HibernateCallback<Boolean>() {
-            @Override
-            public Boolean doInHibernate(Session session) throws HibernateException, SQLException {
-                final SQLQuery sqlQuery = session.createSQLQuery("select " + name + " from where pid=?");
-                sqlQuery.setLong(1, personality.getId());
-                return null;//sqlQuery.uniqueResult();
-            }
-        });
-        throw new UnsupportedOperationException("TODO: not implemented");
-    }
+	@Override
+	public boolean isNotificationEnabled(final String name, final Personality personality) {
+		getHibernateTemplate().execute(new HibernateCallback<Boolean>() {
+			@Override
+			public Boolean doInHibernate(Session session) throws HibernateException, SQLException {
+				final SQLQuery sqlQuery = session.createSQLQuery("select " + name + " from where pid=?");
+				sqlQuery.setLong(1, personality.getId());
+				return null;//sqlQuery.uniqueResult();
+			}
+		});
+		throw new UnsupportedOperationException("TODO: not implemented");
+	}
 
-    @Override
-    public boolean[] isNotificationEnabled(String name, Personality[] personalities) {
-        throw new UnsupportedOperationException("TODO: not implemented");
-    }
+	@Override
+	public boolean[] isNotificationEnabled(String name, Personality[] personalities) {
+		throw new UnsupportedOperationException("TODO: not implemented");
+	}
 
-    @Override
-    public NotificationMask getNotificationMask(Personality personality) {
-        throw new UnsupportedOperationException("TODO: not implemented");
-    }
+	@Override
+	public NotificationMask getNotificationMask(Personality personality) {
+		NotificationMask notificationMask = maskMap.get(personality);
+		if (notificationMask == null) {
+			notificationMask = new NotificationMask();
+			for (NotificationDescription d : descriptionMap.values()) {
+				notificationMask.setEnabled(d.getName(), d.isEnabled());
+			}
+			maskMap.put(personality, notificationMask);
+		}
+		return notificationMask;
+	}
 
-    @Override
-    public void setNotificationMask(Personality personality, NotificationMask notification) {
-        throw new UnsupportedOperationException("TODO: not implemented");
-    }
+	@Override
+	public void setNotificationMask(Personality personality, NotificationMask mask) {
+		maskMap.put(personality, mask);
+	}
 
-    public void setNotificationDescription(Collection<NotificationDescription> descriptions) {
-        final Map<String, NotificationDescription> desc = new HashMap<String, NotificationDescription>();
-        if (descriptions != null) {
-            for (NotificationDescription d : descriptions) {
-                if (desc.put(d.getName(), d) != null) {
-                    throw new IllegalArgumentException("Specified name already contains d for " + d.getName());
-                }
-            }
-        }
-        descriptionMap.clear();
-        descriptionMap.putAll(desc);
-    }
+	public void setNotificationDescription(Collection<NotificationDescription> descriptions) {
+		final Map<String, NotificationDescription> desc = new HashMap<String, NotificationDescription>();
+		if (descriptions != null) {
+			for (NotificationDescription d : descriptions) {
+				if (desc.put(d.getName(), d) != null) {
+					throw new IllegalArgumentException("Specified name already contains d for " + d.getName());
+				}
+			}
+		}
+		descriptionMap.clear();
+		descriptionMap.putAll(desc);
+	}
 }
