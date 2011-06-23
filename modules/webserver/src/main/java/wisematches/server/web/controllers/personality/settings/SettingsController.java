e144829b5a40a916b40213906ab2cdab317763ec
smklimenko <smklimenko@localhost>
2011-06-23 22:16:41 +0400

    ~ Issue 32: 	There are no email notifications    NotificationManager is ready

--- modules/webserver/src/main/java/wisematches/server/web/controllers/personality/settings/SettingsController.java
+++ modules/webserver/src/main/java/wisematches/server/web/controllers/personality/settings/SettingsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import wisematches.personality.Language;
+import wisematches.personality.Personality;
 import wisematches.personality.account.*;
 import wisematches.personality.player.Player;
 import wisematches.server.security.AccountSecurityService;
@@ -19,9 +20,13 @@ import wisematches.server.web.controllers.AbstractPlayerController;
 import wisematches.server.web.controllers.UnknownEntityException;
 import wisematches.server.web.controllers.personality.settings.form.SettingsForm;
 import wisematches.server.web.controllers.personality.settings.form.TimeZoneInfo;
+import wisematches.server.web.services.notice.NotificationDescription;
 import wisematches.server.web.services.notice.NotificationManager;
+import wisematches.server.web.services.notice.NotificationMask;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.Collection;
 import java.util.Set;
 import java.util.TimeZone;
 
@@ -31,142 +36,152 @@ import java.util.TimeZone;
 @Controller
 @RequestMapping("/account/modify")
 public class SettingsController extends AbstractPlayerController {
-	private AccountManager accountManager;
-	private NotificationManager notificationManager;
-	private AccountSecurityService accountSecurityService;
+    private AccountManager accountManager;
+    private NotificationManager notificationManager;
+    private AccountSecurityService accountSecurityService;
 
-	private static final Log log = LogFactory.getLog("wisematches.server.web.settings");
+    private static final Log log = LogFactory.getLog("wisematches.server.web.settings");
 
-	public SettingsController() {
-	}
+    public SettingsController() {
+    }
 
-	@RequestMapping(value = "")
-	public String modifyAccountPage(Model model, @ModelAttribute("settings") SettingsForm form) {
-		final Player principal = getPrincipal();
-		if (principal.getTimeZone() != null) {
-			form.setTimezone(principal.getTimeZone().getID());
-		}
-		form.setLanguage(principal.getLanguage().name().toLowerCase());
-		form.setEmail(principal.getEmail());
-		model.addAttribute("timeZones", TimeZoneInfo.getTimeZones());
-		model.addAttribute("notificationMask", notificationManager.getNotificationMask(principal));
-		model.addAttribute("notificationDescriptions", notificationManager.getDescriptions());
-		return "/content/personality/settings/template";
-	}
+    @RequestMapping(value = "")
+    public String modifyAccountPage(Model model, @ModelAttribute("settings") SettingsForm form) {
+        final Player principal = getPrincipal();
+        if (principal.getTimeZone() != null) {
+            form.setTimezone(principal.getTimeZone().getID());
+        }
+        form.setLanguage(principal.getLanguage().name().toLowerCase());
+        form.setEmail(principal.getEmail());
+        model.addAttribute("timeZones", TimeZoneInfo.getTimeZones());
+        model.addAttribute("notificationMask", notificationManager.getNotificationMask(principal));
+        model.addAttribute("notificationDescriptions", notificationManager.getDescriptions());
+        return "/content/personality/settings/template";
+    }
 
-	@RequestMapping(value = "done")
-	public String modifyAccountDone(Model model, @ModelAttribute("settings") SettingsForm form) {
-		model.addAttribute("saved", Boolean.TRUE);
-		return modifyAccountPage(model, form);
-	}
+    @RequestMapping(value = "done")
+    public String modifyAccountDone(Model model, @ModelAttribute("settings") SettingsForm form) {
+        model.addAttribute("saved", Boolean.TRUE);
+        return modifyAccountPage(model, form);
+    }
 
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	@RequestMapping(value = "save", method = RequestMethod.POST)
-	public String modifyAccountAction(@Valid @ModelAttribute("settings") SettingsForm form,
-									  BindingResult errors, Model model) throws UnknownEntityException {
-		final Account account = accountManager.getAccount(getPersonality().getId());
-		if (account == null) {
-			throw new UnknownEntityException(null, "account");
-		}
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String modifyAccountAction(@Valid @ModelAttribute("settings") SettingsForm form,
+                                      BindingResult errors, Model model, HttpServletRequest request) throws UnknownEntityException {
+        final Personality personality = getPersonality();
+        final Account account = accountManager.getAccount(personality.getId());
+        if (account == null) {
+            throw new UnknownEntityException(null, "account");
+        }
 
-		Language language = account.getLanguage();
-		if (form.getLanguage() != null) {
-			try {
-				language = Language.valueOf(form.getLanguage().toUpperCase());
-			} catch (IllegalArgumentException ex) {
-				errors.rejectValue("language", "account.register.language.err.unknown");
-			}
-		}
 
-		TimeZone timeZone = account.getTimeZone();
-		if (form.getTimezone() != null) {
-			timeZone = TimeZone.getTimeZone(form.getTimezone());
-			if (timeZone == null) {
-				errors.rejectValue("timezone", "account.register.timezone.err.unknown");
-			}
-		}
+        final NotificationMask mask = notificationManager.getNotificationMask(personality);
+        final Collection<NotificationDescription> descriptions = notificationManager.getDescriptions();
+        for (NotificationDescription description : descriptions) {
+            final String parameter = request.getParameter(description.getName());
+            mask.setEnabled(description.getName(), parameter != null && Boolean.parseBoolean(parameter));
+        }
+        notificationManager.setNotificationMask(personality, mask);
 
-		if (form.isChangeEmail() && form.getEmail().trim().isEmpty()) {
-			errors.rejectValue("email", "account.register.email.err.blank");
-		}
+        Language language = account.getLanguage();
+        if (form.getLanguage() != null) {
+            try {
+                language = Language.valueOf(form.getLanguage().toUpperCase());
+            } catch (IllegalArgumentException ex) {
+                errors.rejectValue("language", "account.register.language.err.unknown");
+            }
+        }
 
-		if (form.isChangePassword()) {
-			if (form.getPassword().trim().isEmpty()) {
-				errors.rejectValue("password", "account.register.pwd.err.blank");
-			}
-			if (form.getConfirm().trim().isEmpty()) {
-				errors.rejectValue("confirm", "account.register.pwd-cfr.err.blank");
-			}
+        TimeZone timeZone = account.getTimeZone();
+        if (form.getTimezone() != null) {
+            timeZone = TimeZone.getTimeZone(form.getTimezone());
+            if (timeZone == null) {
+                errors.rejectValue("timezone", "account.register.timezone.err.unknown");
+            }
+        }
 
-			if (!form.getPassword().equals(form.getConfirm())) {
-				errors.rejectValue("confirm", "account.register.pwd-cfr.err.mismatch");
-			}
-		}
+        if (form.isChangeEmail() && form.getEmail().trim().isEmpty()) {
+            errors.rejectValue("email", "account.register.email.err.blank");
+        }
 
-		if (!errors.hasErrors()) {
-			final AccountEditor editor = new AccountEditor(account);
-			if (language != editor.getLanguage()) {
-				editor.setLanguage(language);
-			}
-			if (!editor.getTimeZone().equals(timeZone)) {
-				editor.setTimeZone(timeZone);
-			}
+        if (form.isChangePassword()) {
+            if (form.getPassword().trim().isEmpty()) {
+                errors.rejectValue("password", "account.register.pwd.err.blank");
+            }
+            if (form.getConfirm().trim().isEmpty()) {
+                errors.rejectValue("confirm", "account.register.pwd-cfr.err.blank");
+            }
 
-			if (form.isChangeEmail() && !editor.getEmail().equals(form.getEmail())) {
-				editor.setEmail(form.getEmail());
-			}
+            if (!form.getPassword().equals(form.getConfirm())) {
+                errors.rejectValue("confirm", "account.register.pwd-cfr.err.mismatch");
+            }
+        }
 
-			if (form.isChangePassword()) {
-				if (accountSecurityService != null) {
-					editor.setPassword(accountSecurityService.encodePlayerPassword(editor.createAccount(), form.getPassword()));
-				} else {
-					editor.setPassword(form.getPassword());
-				}
-			}
+        if (!errors.hasErrors()) {
+            final AccountEditor editor = new AccountEditor(account);
+            if (language != editor.getLanguage()) {
+                editor.setLanguage(language);
+            }
+            if (!editor.getTimeZone().equals(timeZone)) {
+                editor.setTimeZone(timeZone);
+            }
 
-			try {
-				final Account a = accountManager.updateAccount(editor.createAccount());
-				if (accountSecurityService != null) {
-					accountSecurityService.authenticatePlayer(a, form.isChangePassword() ? form.getPassword() : null);
-				}
-				return "redirect:/account/modify/done";
-			} catch (UnknownAccountException e) {
-				throw new UnknownEntityException(null, "account");
-			} catch (DuplicateAccountException ex) {
-				final Set<String> fieldNames = ex.getFieldNames();
-				if (fieldNames.contains("email")) {
-					errors.rejectValue("email", "account.register.form.email.err.busy");
-				}
-				if (fieldNames.contains("nickname")) {
-					errors.rejectValue("nickname", "account.register.form.username.err.incorrect");
-				}
-			} catch (InadmissibleUsernameException ex) {
-				errors.rejectValue("username", "account.register.form.username.err.incorrect");
-			} catch (Exception ex) {
-				log.error("Account can't be created", ex);
-				errors.reject("wisematches.error.internal");
-			}
-		}
-		return modifyAccountPage(model, form);
-	}
+            if (form.isChangeEmail() && !editor.getEmail().equals(form.getEmail())) {
+                editor.setEmail(form.getEmail());
+            }
 
-	@ModelAttribute("headerTitle")
-	public String getHeaderTitle() {
-		return "title.settings";
-	}
+            if (form.isChangePassword()) {
+                if (accountSecurityService != null) {
+                    editor.setPassword(accountSecurityService.encodePlayerPassword(editor.createAccount(), form.getPassword()));
+                } else {
+                    editor.setPassword(form.getPassword());
+                }
+            }
 
-	@Autowired
-	public void setAccountManager(AccountManager accountManager) {
-		this.accountManager = accountManager;
-	}
+            try {
+                final Account a = accountManager.updateAccount(editor.createAccount());
+                if (accountSecurityService != null) {
+                    accountSecurityService.authenticatePlayer(a, form.isChangePassword() ? form.getPassword() : null);
+                }
+                return "redirect:/account/modify/done";
+            } catch (UnknownAccountException e) {
+                throw new UnknownEntityException(null, "account");
+            } catch (DuplicateAccountException ex) {
+                final Set<String> fieldNames = ex.getFieldNames();
+                if (fieldNames.contains("email")) {
+                    errors.rejectValue("email", "account.register.form.email.err.busy");
+                }
+                if (fieldNames.contains("nickname")) {
+                    errors.rejectValue("nickname", "account.register.form.username.err.incorrect");
+                }
+            } catch (InadmissibleUsernameException ex) {
+                errors.rejectValue("username", "account.register.form.username.err.incorrect");
+            } catch (Exception ex) {
+                log.error("Account can't be created", ex);
+                errors.reject("wisematches.error.internal");
+            }
+        }
+        return modifyAccountPage(model, form);
+    }
 
-	@Autowired
-	public void setAccountSecurityService(AccountSecurityService accountSecurityService) {
-		this.accountSecurityService = accountSecurityService;
-	}
+    @ModelAttribute("headerTitle")
+    public String getHeaderTitle() {
+        return "title.settings";
+    }
 
-	@Autowired
-	public void setNotificationManager(NotificationManager notificationManager) {
-		this.notificationManager = notificationManager;
-	}
+    @Autowired
+    public void setAccountManager(AccountManager accountManager) {
+        this.accountManager = accountManager;
+    }
+
+    @Autowired
+    public void setAccountSecurityService(AccountSecurityService accountSecurityService) {
+        this.accountSecurityService = accountSecurityService;
+    }
+
+    @Autowired
+    public void setNotificationManager(NotificationManager notificationManager) {
+        this.notificationManager = notificationManager;
+    }
 }
---
+++ modules/webserver/src/test/java/wisematches/server/web/services/notice/impl/HibernateNotificationManagerTest.java
@@ -0,0 +1,70 @@
+package wisematches.server.web.services.notice.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import wisematches.personality.Personality;
+import wisematches.server.web.services.notice.NotificationDescription;
+import wisematches.server.web.services.notice.NotificationManager;
+import wisematches.server.web.services.notice.NotificationMask;
+
+import java.util.Collection;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author Sergey Klimenko (smklimenko@gmail.com)
+ */
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:/config/database-junit-config.xml",
+        "classpath:/config/accounts-config.xml",
+        "classpath:/config/playground-config.xml",
+        "classpath:/config/scribble-junit-config.xml",
+        "classpath:/config/application-settings.xml",
+        "classpath:/config/server-web-config.xml"
+})
+public class HibernateNotificationManagerTest {
+    @Autowired
+    NotificationManager notificationManager;
+
+    public HibernateNotificationManagerTest() {
+    }
+
+    @Test
+    public void test() {
+        final Personality person = Personality.person(1);
+
+        NotificationMask notificationMask = notificationManager.getNotificationMask(person);
+        for (String name : notificationMask.getNotificationNames()) {
+            notificationMask.setEnabled(name, true);
+        }
+        notificationManager.setNotificationMask(person, notificationMask);
+
+        final Collection<NotificationDescription> descriptions = notificationManager.getDescriptions();
+        for (NotificationDescription d : descriptions) {
+            assertTrue(notificationManager.isNotificationEnabled(d.getName(), person));
+        }
+
+        notificationMask = notificationManager.getNotificationMask(person);
+        System.out.println(notificationMask);
+        for (String name : notificationMask.getNotificationNames()) {
+            assertTrue(notificationMask.isEnabled(name));
+            notificationMask.setEnabled(name, false);
+        }
+        notificationManager.setNotificationMask(person, notificationMask);
+
+        for (NotificationDescription d : descriptions) {
+            assertFalse(notificationManager.isNotificationEnabled(d.getName(), person));
+        }
+        notificationMask = notificationManager.getNotificationMask(person);
+        for (String name : notificationMask.getNotificationNames()) {
+            assertFalse(notificationMask.isEnabled(name));
+        }
+    }
+}
