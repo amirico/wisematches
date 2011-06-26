7aa10761ab461de1b303f62f589bad45f69542e4
smklimenko <smklimenko@localhost>
2011-06-26 23:33:34 +0400

    ~ Issue 32: 	There are no email notifications ~ Issue 16:	No messages page

--- modules/webserver/src/main/java/wisematches/server/mail/MailSender.java
+++ modules/webserver/src/main/java/wisematches/server/mail/MailSender.java
@@ -1,36 +1,36 @@
 package wisematches.server.mail;
 
 /**
- * From addresses enum. This enum contains type of emails messages can be sent from.
+ * From addresses enum. This enum contains type of emails message can be sent from.
  */
 public enum MailSender {
-	/**
-	 * This is abstract e-mail notification.
-	 */
-	UNDEFINED("noreplay"),
+    /**
+     * This is abstract e-mail notification.
+     */
+    UNDEFINED("noreplay"),
 
-	/**
-	 * From address is bugs reporter.
-	 */
-	SUPPORT("support"),
+    /**
+     * From address is bugs reporter.
+     */
+    SUPPORT("support"),
 
-	/**
-	 * Mail was sent from game state notifications team
-	 */
-	GAME("game-noreplay"),
+    /**
+     * Mail was sent from game state notifications team
+     */
+    GAME("game-noreplay"),
 
-	/**
-	 * Mail was sent from accounts support team.
-	 */
-	ACCOUNTS("account-noreplay");
+    /**
+     * Mail was sent from accounts support team.
+     */
+    ACCOUNTS("account-noreplay");
 
-	private final String defaultName;
+    private final String defaultName;
 
-	MailSender(String defaultName) {
-		this.defaultName = defaultName;
-	}
+    MailSender(String defaultName) {
+        this.defaultName = defaultName;
+    }
 
-	public String getDefaultName() {
-		return defaultName;
-	}
+    public String getDefaultName() {
+        return defaultName;
+    }
 }
---
+++ modules/webserver/src/main/java/wisematches/server/web/services/notice/publisher/NotificationPublishCenter.java
@@ -0,0 +1,241 @@
+package wisematches.server.web.services.notice.publisher;
+
+import wisematches.personality.Personality;
+import wisematches.personality.player.computer.ComputerPlayer;
+import wisematches.playground.*;
+import wisematches.playground.expiration.GameExpirationListener;
+import wisematches.playground.expiration.GameExpirationManager;
+import wisematches.playground.expiration.GameExpirationType;
+import wisematches.playground.message.Message;
+import wisematches.playground.message.MessageListener;
+import wisematches.server.web.services.notice.NotificationDescription;
+import wisematches.server.web.services.notice.NotificationManager;
+import wisematches.server.web.services.state.PlayerStateListener;
+import wisematches.server.web.services.state.PlayerStateManager;
+
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author Sergey Klimenko (smklimenko@gmail.com)
+ */
+public class NotificationPublishCenter {
+    private BoardManager boardManager;
+    private PlayerStateManager playerStateManager;
+    private GameExpirationManager expirationManager;
+    private NotificationManager notificationManager;
+
+    private final Lock lock = new ReentrantLock();
+    private final Map<Personality, List<Notification>> bufferedNotification = new HashMap<Personality, List<Notification>>();
+
+    private final PlayerStateListener stateListener = new ThePlayerStateListener();
+    private final TheNotificationListener notificationListener = new TheNotificationListener();
+    private final Collection<NotificationPublisher> publishers = new ArrayList<NotificationPublisher>();
+
+    public NotificationPublishCenter() {
+    }
+
+    public void processNotification(Notification notification) {
+        final NotificationDescription description = notification.getDescription();
+        final Personality personality = notification.getPersonality();
+        if (personality instanceof ComputerPlayer) {
+            return;
+        }
+
+        if (notificationManager.isNotificationEnabled(description.getName(), personality)) {
+            if (description.isEvenOnline()) {
+                fireNotification(notification);
+            } else {
+                if (!playerStateManager.isPlayerOnline(personality)) {
+                    fireNotification(notification);
+                } else {
+                    lock.lock();
+                    try {
+                        List<Notification> notifications = bufferedNotification.get(personality);
+                        if (notifications == null) {
+                            notifications = new ArrayList<Notification>();
+                            bufferedNotification.put(personality, notifications);
+                        }
+                        notifications.add(notification);
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+    }
+
+    public void processNotifications(Collection<Notification> notifications) {
+        final Set<String> series = new HashSet<String>();
+
+        for (Notification n : notifications) {
+            final NotificationDescription d = n.getDescription();
+            final String s = d.getSeries();
+            if (s != null && !s.isEmpty()) {
+                if (series.contains(s)) {
+                    continue;
+                }
+                series.add(s);
+            }
+            processNotification(n);
+        }
+    }
+
+
+    protected void fireNotification(Notification notification) {
+        for (NotificationPublisher publisher : publishers) {
+            publisher.publishNotification(notification);
+        }
+    }
+
+
+    public void setPublishers(Collection<NotificationPublisher> publishers) {
+        this.publishers.clear();
+
+        if (publishers != null) {
+            this.publishers.addAll(publishers);
+        }
+    }
+
+    public void setPlayerStateManager(PlayerStateManager playerStateManager) {
+        if (this.playerStateManager != null) {
+            this.playerStateManager.removePlayerStateListener(stateListener);
+        }
+
+        this.playerStateManager = playerStateManager;
+
+        if (this.playerStateManager != null) {
+            this.playerStateManager.addPlayerStateListener(stateListener);
+        }
+    }
+
+    public void setNotificationManager(NotificationManager notificationManager) {
+        this.notificationManager = notificationManager;
+    }
+
+
+    public void setBoardManager(BoardManager boardManager) {
+        if (this.boardManager != null) {
+            this.boardManager.removeBoardStateListener(notificationListener);
+        }
+        this.boardManager = boardManager;
+        if (this.boardManager != null) {
+            this.boardManager.addBoardStateListener(notificationListener);
+        }
+    }
+
+    public void setExpirationManager(GameExpirationManager expirationManager) {
+        if (this.expirationManager != null) {
+            this.expirationManager.removeGameExpirationListener(notificationListener);
+        }
+        this.expirationManager = expirationManager;
+        if (this.expirationManager != null) {
+            this.expirationManager.addGameExpirationListener(notificationListener);
+        }
+    }
+
+    private class ThePlayerStateListener implements PlayerStateListener {
+        private ThePlayerStateListener() {
+        }
+
+        @Override
+        public void playerOnline(Personality person) {
+        }
+
+        @Override
+        public void playerAlive(Personality person) {
+            lock.lock();
+            try {
+                bufferedNotification.remove(person);
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        @Override
+        public void playerOffline(Personality person) {
+            final List<Notification> remove;
+            lock.lock();
+            try {
+                remove = bufferedNotification.remove(person);
+            } finally {
+                lock.unlock();
+            }
+            if (remove != null) {
+                processNotifications(remove);
+            }
+        }
+    }
+
+    private class TheNotificationListener implements BoardStateListener, GameExpirationListener, MessageListener {
+        private TheNotificationListener() {
+        }
+
+        @Override
+        public void gameStarted(GameBoard<? extends GameSettings, ? extends GamePlayerHand> board) {
+            final NotificationDescription description = notificationManager.getDescription("game.started");
+            if (description != null) {
+                final Collection<? extends GamePlayerHand> playersHands = board.getPlayersHands();
+                for (GamePlayerHand hand : playersHands) {
+                    processNotification(new Notification(Personality.person(hand.getPlayerId()), description, board));
+                }
+            }
+        }
+
+        @Override
+        public void gameMoveDone(GameBoard<? extends GameSettings, ? extends GamePlayerHand> board, GameMove move) {
+            final NotificationDescription d1 = notificationManager.getDescription("game.move.your");
+            final NotificationDescription d2 = notificationManager.getDescription("game.move.opponent");
+            final Collection<? extends GamePlayerHand> playersHands = board.getPlayersHands();
+            for (GamePlayerHand hand : playersHands) {
+                if (board.getPlayerTurn() != null && board.getPlayerTurn().equals(hand)) {
+                    if (d1 != null) {
+                        processNotification(new Notification(Personality.person(hand.getPlayerId()), d1, board));
+                    }
+                } else {
+                    if (d2 != null) {
+                        processNotification(new Notification(Personality.person(hand.getPlayerId()), d2, board));
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void gameFinished(GameBoard<? extends GameSettings, ? extends GamePlayerHand> board, GameResolution resolution, Collection<? extends GamePlayerHand> wonPlayers) {
+            final NotificationDescription description = notificationManager.getDescription("game.finished");
+            if (description != null) {
+                final Collection<? extends GamePlayerHand> playersHands = board.getPlayersHands();
+                for (GamePlayerHand hand : playersHands) {
+                    processNotification(new Notification(Personality.person(hand.getPlayerId()), description, board));
+                }
+            }
+        }
+
+        @Override
+        public void gameExpiring(long boardId, GameExpirationType expiration) {
+            try {
+                final GameBoard board = boardManager.openBoard(boardId);
+                if (board != null) {
+                    final GamePlayerHand hand = board.getPlayerTurn();
+                    if (hand != null) {
+                        NotificationDescription description = notificationManager.getDescription(expiration.getCode());
+                        if (description != null) {
+                            processNotification(new Notification(Personality.person(hand.getPlayerId()), description, board));
+                        }
+                    }
+                }
+            } catch (BoardLoadingException ex) {
+                ;
+            }
+        }
+
+        @Override
+        public void messageSent(Message message) {
+            final NotificationDescription description = notificationManager.getDescription("message");
+            if (description != null) {
+                processNotification(new Notification(Personality.person(message.getRecipient()), description, message));
+            }
+        }
+    }
+}
