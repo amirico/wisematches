0467f1662902104f2e60271e569404c5aa0c0da8
smklimenko <smklimenko@localhost>
2011-07-14 08:53:54 +0400

    ~ Issue 16: 	No messages page

--- modules/webserver/src/main/java/wisematches/server/web/controllers/playground/form/MessageForm.java
+++ modules/webserver/src/main/java/wisematches/server/web/controllers/playground/form/MessageForm.java
@@ -1,46 +1,49 @@
 package wisematches.server.web.controllers.playground.form;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 /**
  * @author Sergey Klimenko (smklimenko@gmail.com)
  */
 public class MessageForm {
-	private String msgText;
-	private long msgBoard;
-	private long msgOriginal;
-	private long msgRecipient;
+    @NotEmpty(message = "asdasd")
+    private String msgText;
+    private long msgBoard;
+    private long msgOriginal;
+    private long msgRecipient;
 
-	public MessageForm() {
-	}
+    public MessageForm() {
+    }
 
-	public long getMsgRecipient() {
-		return msgRecipient;
-	}
+    public long getMsgRecipient() {
+        return msgRecipient;
+    }
 
-	public void setMsgRecipient(long msgRecipient) {
-		this.msgRecipient = msgRecipient;
-	}
+    public void setMsgRecipient(long msgRecipient) {
+        this.msgRecipient = msgRecipient;
+    }
 
-	public String getMsgText() {
-		return msgText;
-	}
+    public String getMsgText() {
+        return msgText;
+    }
 
-	public void setMsgText(String msgText) {
-		this.msgText = msgText;
-	}
+    public void setMsgText(String msgText) {
+        this.msgText = msgText;
+    }
 
-	public long getMsgOriginal() {
-		return msgOriginal;
-	}
+    public long getMsgOriginal() {
+        return msgOriginal;
+    }
 
-	public void setMsgOriginal(long msgOriginal) {
-		this.msgOriginal = msgOriginal;
-	}
+    public void setMsgOriginal(long msgOriginal) {
+        this.msgOriginal = msgOriginal;
+    }
 
-	public long getMsgBoard() {
-		return msgBoard;
-	}
+    public long getMsgBoard() {
+        return msgBoard;
+    }
 
-	public void setMsgBoard(long msgBoard) {
-		this.msgBoard = msgBoard;
-	}
+    public void setMsgBoard(long msgBoard) {
+        this.msgBoard = msgBoard;
+    }
 }
