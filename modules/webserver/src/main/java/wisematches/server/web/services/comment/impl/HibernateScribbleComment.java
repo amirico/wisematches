c92594d00a1a06df54ad15d48542eac4a4dead33
smklimenko <smklimenko@localhost>
2011-07-25 12:27:46 +0400

    HibernateScribbleCommentManager is ready

--- modules/webserver/src/main/java/wisematches/server/web/services/comment/impl/HibernateScribbleComment.java
+++ modules/webserver/src/main/java/wisematches/server/web/services/comment/impl/HibernateScribbleComment.java
@@ -4,10 +4,7 @@ import wisematches.personality.player.Player;
 import wisematches.playground.scribble.ScribbleBoard;
 import wisematches.server.web.services.comment.ScribbleComment;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 /**
@@ -16,54 +13,55 @@ import java.util.Date;
 @Entity
 @Table(name = "scribble_comment")
 public class HibernateScribbleComment implements ScribbleComment {
-    @Id
-    @Column(name = "id")
-    private byte id;
+	@Id
+	@Column(name = "id", nullable = false, updatable = false, unique = true)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-    @Column(name = "board")
-    private long board;
+	@Column(name = "board")
+	private long board;
 
-    @Column(name = "person")
-    private long person;
+	@Column(name = "person")
+	private long person;
 
-    @Column(name = "text")
-    private String text;
+	@Column(name = "text")
+	private String text;
 
-    @Column(name = "creationDate")
-    private Date creationDate;
+	@Column(name = "creationDate")
+	private Date creationDate;
 
-    public HibernateScribbleComment() {
-    }
+	public HibernateScribbleComment() {
+	}
 
-    public HibernateScribbleComment(Player person, ScribbleBoard board, String text) {
-        this.board = board.getBoardId();
-        this.person = person.getId();
-        this.text = text;
-        this.creationDate = new Date();
-    }
+	public HibernateScribbleComment(Player person, ScribbleBoard board, String text) {
+		this.board = board.getBoardId();
+		this.person = person.getId();
+		this.text = text;
+		this.creationDate = new Date();
+	}
 
-    @Override
-    public long getId() {
-        return id;
-    }
+	@Override
+	public long getId() {
+		return id;
+	}
 
-    @Override
-    public long getBoard() {
-        return board;
-    }
+	@Override
+	public long getBoard() {
+		return board;
+	}
 
-    @Override
-    public long getPerson() {
-        return person;
-    }
+	@Override
+	public long getPerson() {
+		return person;
+	}
 
-    @Override
-    public String getText() {
-        return text;
-    }
+	@Override
+	public String getText() {
+		return text;
+	}
 
-    @Override
-    public Date getCreationDate() {
-        return creationDate;
-    }
+	@Override
+	public Date getCreationDate() {
+		return creationDate;
+	}
 }
