6377c523774a1e0e4fc57ede70750823d6446e10
smklimenko <smklimenko@localhost>
2011-07-25 12:27:46 +0400

    HibernateScribbleCommentManager is ready

--- modules/webserver/src/main/java/wisematches/server/web/services/comment/impl/HibernateScribbleCommentManager.java
+++ modules/webserver/src/main/java/wisematches/server/web/services/comment/impl/HibernateScribbleCommentManager.java
@@ -1,36 +1,80 @@
 package wisematches.server.web.services.comment.impl;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import wisematches.personality.player.Player;
 import wisematches.playground.scribble.ScribbleBoard;
 import wisematches.server.web.services.comment.ScribbleComment;
 import wisematches.server.web.services.comment.ScribbleCommentManager;
 
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * @author Sergey Klimenko (smklimenko@gmail.com)
  */
 public class HibernateScribbleCommentManager extends HibernateDaoSupport implements ScribbleCommentManager {
 
-    public HibernateScribbleCommentManager() {
-    }
+	private static final long[] EMPTY_ARRAY = new long[0];
 
-    @Override
-    public void addComment(Player player, ScribbleBoard board, String text) {
-        getHibernateTemplate().saveOrUpdate(new HibernateScribbleComment(player, board, text));
-    }
+	public HibernateScribbleCommentManager() {
+	}
 
-    @Override
-    public void removeComment(Player player, ScribbleBoard board, long commentId) {
-        throw new UnsupportedOperationException("TODO: not implemented");
-    }
+	@Override
+	public void addComment(Player player, ScribbleBoard board, String text) {
+		getHibernateTemplate().save(new HibernateScribbleComment(player, board, text));
+	}
 
-    @Override
-    public long[] getBoardComments(ScribbleBoard board) {
-        throw new UnsupportedOperationException("TODO: not implemented");
-    }
+	@Override
+	public void removeComment(Player player, ScribbleBoard board, long commentId) {
+		HibernateTemplate template = getHibernateTemplate();
+		HibernateScribbleComment comment = template.get(HibernateScribbleComment.class, commentId);
+		if (comment != null) {
+			if (comment.getPerson() != player.getId()) {
+				throw new IllegalArgumentException("Comment with id " + commentId + " belongs to another player");
+			}
+			if (comment.getBoard() != board.getBoardId()) {
+				throw new IllegalArgumentException("Comment with id " + commentId + " belongs to another board");
+			}
+			template.delete(comment);
+		} else {
+			throw new IllegalArgumentException("There is no comment with id " + commentId);
+		}
+	}
 
-    @Override
-    public ScribbleComment getComment(long commentId) {
-        throw new UnsupportedOperationException("TODO: not implemented");
-    }
+	@Override
+	public long[] getBoardComments(ScribbleBoard board) {
+		List l = getHibernateTemplate().find("select id from wisematches.server.web.services.comment.impl.HibernateScribbleComment where board=? order by creationDate, id desc", board.getBoardId());
+		if (l.size() == 0) {
+			return EMPTY_ARRAY;
+		}
+
+		int i = 0;
+		final long[] res = new long[l.size()];
+		for (Object o : l) {
+			res[i++] = (Long) o;
+		}
+		return res;
+	}
+
+	@Override
+	public ScribbleComment getComment(long commentId) {
+		return getHibernateTemplate().get(HibernateScribbleComment.class, commentId);
+	}
+
+	@Override
+	public void clearComments(final ScribbleBoard board) {
+		getHibernateTemplate().execute(new HibernateCallback<Object>() {
+			@Override
+			public Object doInHibernate(Session session) throws HibernateException, SQLException {
+				final Query query = session.createQuery("delete wisematches.server.web.services.comment.impl.HibernateScribbleComment where board=?");
+				query.setLong(0, board.getBoardId());
+				return query.executeUpdate();
+			}
+		});
+	}
 }
--- modules/webserver/src/test/java/wisematches/server/web/services/comment/impl/HibernateScribbleCommentManagerTest.java
+++ modules/webserver/src/test/java/wisematches/server/web/services/comment/impl/HibernateScribbleCommentManagerTest.java
@@ -7,10 +7,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 import wisematches.personality.player.Player;
-import wisematches.personality.player.computer.guest.GuestPlayer;
+import wisematches.personality.player.computer.robot.RobotPlayer;
 import wisematches.playground.scribble.ScribbleBoard;
+import wisematches.server.web.services.comment.ScribbleComment;
 import wisematches.server.web.services.comment.ScribbleCommentManager;
 
+import static junit.framework.Assert.*;
 import static org.easymock.EasyMock.*;
 
 /**
@@ -19,32 +21,57 @@ import static org.easymock.EasyMock.*;
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
-        "classpath:/config/database-junit-config.xml",
-        "classpath:/config/accounts-config.xml",
-        "classpath:/config/playground-config.xml",
-        "classpath:/config/scribble-junit-config.xml",
-        "classpath:/config/application-settings.xml",
-        "classpath:/config/server-web-junit-config.xml"
+		"classpath:/config/database-junit-config.xml",
+		"classpath:/config/accounts-config.xml",
+		"classpath:/config/playground-config.xml",
+		"classpath:/config/scribble-junit-config.xml",
+		"classpath:/config/application-settings.xml",
+		"classpath:/config/server-web-junit-config.xml"
 })
 public class HibernateScribbleCommentManagerTest {
-    @Autowired
-    private ScribbleCommentManager commentManager;
+	@Autowired
+	private ScribbleCommentManager commentManager;
 
-    public HibernateScribbleCommentManagerTest() {
-    }
+	public HibernateScribbleCommentManagerTest() {
+	}
 
-    @Test
-    public void test() {
-        final Player player = GuestPlayer.GUEST;
+	@Test
+	public void test() {
+		final Player player1 = RobotPlayer.DULL;
+		final Player player2 = RobotPlayer.EXPERT;
 
-        final ScribbleBoard board = createMock(ScribbleBoard.class);
-        expect(board.getBoardId()).andReturn(2L);
-        replay(board);
+		final ScribbleBoard board = createMock(ScribbleBoard.class);
+		expect(board.getBoardId()).andReturn(2L).times(8);
+		replay(board);
 
-        commentManager.addComment(player, board, "This is message");
+		commentManager.addComment(player1, board, "This is message1");
+		commentManager.addComment(player2, board, "This is message2");
 
-        verify(board);
+		final long[] boardComments = commentManager.getBoardComments(board);
+		assertEquals(2, boardComments.length);
 
-        throw new UnsupportedOperationException("Not implemented");
-    }
+		assertComment(commentManager.getComment(boardComments[0]), player2, 2L, "This is message2");
+		assertComment(commentManager.getComment(boardComments[1]), player1, 2L, "This is message1");
+		try {
+			commentManager.removeComment(player1, board, boardComments[0]);
+			fail("Exception must be here");
+		} catch (IllegalArgumentException ex) {
+			assertEquals(2, commentManager.getBoardComments(board).length);
+		}
+
+		commentManager.removeComment(player1, board, boardComments[1]);
+		assertEquals(1, commentManager.getBoardComments(board).length);
+
+		commentManager.clearComments(board);
+		assertEquals(0, commentManager.getBoardComments(board).length);
+
+		verify(board);
+	}
+
+	private void assertComment(ScribbleComment c, Player p, long board, String text) {
+		assertEquals(p.getId(), c.getPerson());
+		assertEquals(board, c.getBoard());
+		assertEquals(text, c.getText());
+		assertNotNull(c.getCreationDate());
+	}
 }
