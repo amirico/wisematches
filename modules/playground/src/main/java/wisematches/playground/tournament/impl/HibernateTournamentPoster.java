3a6a8c83cf35a624c288cb5cadb0d47dacacc0a1
smklimenko <smklimenko@localhost>
2011-12-25 09:46:55 +0400

    project structure and tests are fixed

--- modules/playground/src/main/java/wisematches/playground/tournament/impl/HibernateTournamentPoster.java
+++ modules/playground/src/main/java/wisematches/playground/tournament/impl/HibernateTournamentPoster.java
@@ -11,63 +11,62 @@ import java.util.Date;
 @Entity
 @Table(name = "tournament_poster")
 public class HibernateTournamentPoster implements TournamentPoster {
-	@Id
-	@Column(name = "number")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int number;
+    @Id
+    @Column(name = "number")
+    private int number;
 
-	@Column(name = "scheduled")
-	@Temporal(TemporalType.DATE)
-	private Date scheduledDate;
+    @Column(name = "scheduled")
+    @Temporal(TemporalType.DATE)
+    private Date scheduledDate;
 
-	@Column(name = "started")
-	private boolean started = false;
+    @Column(name = "started")
+    private boolean started = false;
 
-	@Column(name = "processed")
-	private boolean processed = false;
+    @Column(name = "processed")
+    private boolean processed = false;
 
-	@Deprecated
-	HibernateTournamentPoster() {
-	}
+    @Deprecated
+    HibernateTournamentPoster() {
+    }
 
-	HibernateTournamentPoster(int number, Date scheduledDate) {
-		this.number = number;
-		this.scheduledDate = scheduledDate;
-	}
+    HibernateTournamentPoster(int number, Date scheduledDate) {
+        this.number = number;
+        this.scheduledDate = scheduledDate;
+    }
 
-	@Override
-	public int getNumber() {
-		return number;
-	}
+    @Override
+    public int getNumber() {
+        return number;
+    }
 
-	@Override
-	public Date getScheduledDate() {
-		return scheduledDate;
-	}
+    @Override
+    public Date getScheduledDate() {
+        return scheduledDate;
+    }
 
-	@Override
-	public boolean isStarted() {
-		return started;
-	}
+    @Override
+    public boolean isStarted() {
+        return started;
+    }
 
-	void setStarted(boolean started) {
-		this.started = started;
-	}
+    void setStarted(boolean started) {
+        this.started = started;
+    }
 
-	boolean isProcessed() {
-		return processed;
-	}
+    boolean isProcessed() {
+        return processed;
+    }
 
-	void setProcessed(boolean processed) {
-		this.processed = processed;
-	}
+    void setProcessed(boolean processed) {
+        this.processed = processed;
+    }
 
-	@Override
-	public String toString() {
-		return "HibernateTournamentPoster{" +
-				"number=" + number +
-				", scheduledDate=" + scheduledDate +
-				", started=" + started +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "HibernateTournamentPoster{" +
+                "number=" + number +
+                ", scheduledDate=" + scheduledDate +
+                ", started=" + started +
+                '}';
+    }
 }
