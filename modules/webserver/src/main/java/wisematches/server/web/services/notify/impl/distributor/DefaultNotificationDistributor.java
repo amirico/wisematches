ca1faea071f54e4bff450546a41baf8fc3452f66
smklimenko <smklimenko@localhost>
2012-05-04 15:04:58 +0400

    + Issue 172:	DefaultNotificationDistributor: NullPointerException

--- modules/webserver/src/main/java/wisematches/server/web/services/notify/impl/distributor/DefaultNotificationDistributor.java
+++ modules/webserver/src/main/java/wisematches/server/web/services/notify/impl/distributor/DefaultNotificationDistributor.java
@@ -19,242 +19,245 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author Sergey Klimenko (smklimenko@gmail.com)
  */
 public class DefaultNotificationDistributor implements NotificationDistributor {
-    private TaskExecutor taskExecutor;
-    private PlayerStateManager playerStateManager;
-    private NotificationManager notificationManager;
-    private NotificationPublisher internalPublisher;
-    private NotificationPublisher externalPublisher;
+	private TaskExecutor taskExecutor;
+	private PlayerStateManager playerStateManager;
+	private NotificationManager notificationManager;
+	private NotificationPublisher internalPublisher;
+	private NotificationPublisher externalPublisher;
 
-    private final Lock lock = new ReentrantLock();
-    private final ThePlayerStateListener stateListener = new ThePlayerStateListener();
+	private final Lock lock = new ReentrantLock();
+	private final ThePlayerStateListener stateListener = new ThePlayerStateListener();
 
-    private final Set<String> redundantNotifications = new HashSet<String>();
-    private final Set<String> mandatoryNotifications = new HashSet<String>();
-    private final Map<Personality, Collection<Notification>> waitingNotifications = new HashMap<Personality, Collection<Notification>>();
-    private final Collection<NotificationDistributorListener> listeners = new CopyOnWriteArraySet<NotificationDistributorListener>();
+	private final Set<String> redundantNotifications = new HashSet<String>();
+	private final Set<String> mandatoryNotifications = new HashSet<String>();
+	private final Map<Personality, Collection<Notification>> waitingNotifications = new HashMap<Personality, Collection<Notification>>();
+	private final Collection<NotificationDistributorListener> listeners = new CopyOnWriteArraySet<NotificationDistributorListener>();
 
-    private static final Log log = LogFactory.getLog("wisematches.server.notify.distributor");
+	private static final Log log = LogFactory.getLog("wisematches.server.notify.distributor");
 
-    public DefaultNotificationDistributor() {
-    }
+	public DefaultNotificationDistributor() {
+	}
 
-    @Override
-    public void addNotificationDistributorListener(NotificationDistributorListener l) {
-        if (l != null) {
-            listeners.add(l);
-        }
-    }
+	@Override
+	public void addNotificationDistributorListener(NotificationDistributorListener l) {
+		if (l != null) {
+			listeners.add(l);
+		}
+	}
 
-    @Override
-    public void removeNotificationDistributorListener(NotificationDistributorListener l) {
-        listeners.remove(l);
-    }
+	@Override
+	public void removeNotificationDistributorListener(NotificationDistributorListener l) {
+		listeners.remove(l);
+	}
 
-    @Override
-    public void raiseNotification(String code, MemberPlayer recipient, NotificationSender sender, Object context) {
-        raiseNotification(code, recipient.getAccount(), sender, context);
-    }
+	@Override
+	public void raiseNotification(String code, MemberPlayer recipient, NotificationSender sender, Object context) {
+		raiseNotification(code, recipient.getAccount(), sender, context);
+	}
 
-    @Override
-    public void raiseNotification(final String code, final Account recipient, final NotificationSender sender, final Object context) {
-        final NotificationDescriptor descriptor = notificationManager.getDescriptor(code);
-        if (descriptor == null) {
-            throw new IllegalArgumentException("There is no notification with code " + code);
-        }
-        taskExecutor.execute(new Runnable() {
-            @Override
-            public void run() {
-                lock.lock();
-                try {
-                    processNotification(new Notification(descriptor.getCode(), descriptor.getTemplate(), recipient, sender, context));
-                } finally {
-                    lock.unlock();
-                }
-            }
-        });
-    }
-
-
-    protected void processNotification(Notification notification) {
-        processInternalNotification(notification);
-        processExternalNotification(notification);
-    }
-
-    protected void processInternalNotification(Notification notification) {
-        publishNotification(notification, PublicationType.INTERNAL);
-    }
-
-    protected void processExternalNotification(Notification notification) {
-        final String code = notification.getCode();
-        final Personality recipient = notification.getRecipient();
-        if (notificationManager.isNotificationEnabled(recipient, code)) {
-            if (playerStateManager.isPlayerOnline(recipient)) {
-                if (mandatoryNotifications.contains(code)) {
-                    publishNotification(notification, PublicationType.EXTERNAL);
-                } else {
-                    if (redundantNotifications.contains(code)) {
-                        for (Notification n : waitingNotifications.get(recipient)) {
-                            if (code.equals(n.getCode())) {
-                                return; // ignore
-                            }
-                        }
-                        postponeNotification(notification);
-                    } else {
-                        postponeNotification(notification);
-                    }
-                }
-            } else {
-                if (redundantNotifications.contains(code)) {
-                    final Date activityDate = playerStateManager.getLastActivityDate(recipient);
-                    final Date notificationDate = notificationManager.getNotificationDate(recipient, code);
-                    if (notificationDate == null || (activityDate != null && notificationDate.before(activityDate))) {
-                        publishNotification(notification, PublicationType.EXTERNAL);
-                    } else {
-                        return; // ignore
-                    }
-                } else {
-                    publishNotification(notification, PublicationType.EXTERNAL);
-                }
-            }
-        } else {
-            log.debug("Notification '" + code + "' was ignored: disabled by player");
-        }
-    }
+	@Override
+	public void raiseNotification(final String code, final Account recipient, final NotificationSender sender, final Object context) {
+		final NotificationDescriptor descriptor = notificationManager.getDescriptor(code);
+		if (descriptor == null) {
+			throw new IllegalArgumentException("There is no notification with code " + code);
+		}
+		taskExecutor.execute(new Runnable() {
+			@Override
+			public void run() {
+				lock.lock();
+				try {
+					processNotification(new Notification(descriptor.getCode(), descriptor.getTemplate(), recipient, sender, context));
+				} finally {
+					lock.unlock();
+				}
+			}
+		});
+	}
 
 
-    private void postponeNotification(final Notification notification) {
-        Collection<Notification> notifications = waitingNotifications.get(notification.getRecipient());
-        if (notifications == null) {
-            notifications = new ArrayList<Notification>();
-            waitingNotifications.put(notification.getRecipient(), notifications);
-        }
-        notifications.add(notification);
-    }
+	protected void processNotification(Notification notification) {
+		processInternalNotification(notification);
+		processExternalNotification(notification);
+	}
 
-    private void processUnpublishedNotifications(final Personality person) {
-        final Collection<Notification> notifications = waitingNotifications.remove(person);
-        if (notifications != null) {
-            taskExecutor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    for (Notification notification : notifications) {
-                        publishNotification(notification, PublicationType.EXTERNAL);
-                    }
-                }
-            });
-        }
-    }
+	protected void processInternalNotification(Notification notification) {
+		publishNotification(notification, PublicationType.INTERNAL);
+	}
 
-    private void publishNotification(final Notification notification, final PublicationType type) {
-        try {
-            boolean res = false;
-            if (type == PublicationType.EXTERNAL) {
-                if (externalPublisher != null) {
-                    res = externalPublisher.publishNotification(notification);
-                } else {
-                    return;
-                }
-            } else if (type == PublicationType.INTERNAL) {
-                if (internalPublisher != null) {
-                    res = internalPublisher.publishNotification(notification);
-                } else {
-                    return;
-                }
-            } else {
-                throw new UnsupportedOperationException("Publisher type is not supported: " + type);
-            }
-
-            if (res) {
-                for (NotificationDistributorListener listener : listeners) {
-                    listener.notificationPublished(notification, type);
-                }
-            } else {
-                for (NotificationDistributorListener listener : listeners) {
-                    listener.notificationRejected(notification, type);
-                }
-            }
-        } catch (Exception e) {
-            log.error("External notification can't be sent: " + notification, e);
-        }
-    }
+	protected void processExternalNotification(Notification notification) {
+		final String code = notification.getCode();
+		final Personality recipient = notification.getRecipient();
+		if (notificationManager.isNotificationEnabled(recipient, code)) {
+			if (playerStateManager.isPlayerOnline(recipient)) {
+				if (mandatoryNotifications.contains(code)) {
+					publishNotification(notification, PublicationType.EXTERNAL);
+				} else {
+					if (redundantNotifications.contains(code)) {
+						final Collection<Notification> notifications = waitingNotifications.get(recipient);
+						if (notifications != null) {
+							for (Notification n : notifications) {
+								if (code.equals(n.getCode())) {
+									return; // ignore
+								}
+							}
+						}
+						postponeNotification(notification);
+					} else {
+						postponeNotification(notification);
+					}
+				}
+			} else {
+				if (redundantNotifications.contains(code)) {
+					final Date activityDate = playerStateManager.getLastActivityDate(recipient);
+					final Date notificationDate = notificationManager.getNotificationDate(recipient, code);
+					if (notificationDate == null || (activityDate != null && notificationDate.before(activityDate))) {
+						publishNotification(notification, PublicationType.EXTERNAL);
+					} else {
+						return; // ignore
+					}
+				} else {
+					publishNotification(notification, PublicationType.EXTERNAL);
+				}
+			}
+		} else {
+			log.debug("Notification '" + code + "' was ignored: disabled by player");
+		}
+	}
 
 
-    public void setTaskExecutor(TaskExecutor taskExecutor) {
-        this.taskExecutor = taskExecutor;
-    }
+	private void postponeNotification(final Notification notification) {
+		Collection<Notification> notifications = waitingNotifications.get(notification.getRecipient());
+		if (notifications == null) {
+			notifications = new ArrayList<Notification>();
+			waitingNotifications.put(notification.getRecipient(), notifications);
+		}
+		notifications.add(notification);
+	}
 
-    public void setPlayerStateManager(PlayerStateManager playerStateManager) {
-        if (this.playerStateManager != null) {
-            this.playerStateManager.removePlayerStateListener(stateListener);
-        }
+	private void processUnpublishedNotifications(final Personality person) {
+		final Collection<Notification> notifications = waitingNotifications.remove(person);
+		if (notifications != null) {
+			taskExecutor.execute(new Runnable() {
+				@Override
+				public void run() {
+					for (Notification notification : notifications) {
+						publishNotification(notification, PublicationType.EXTERNAL);
+					}
+				}
+			});
+		}
+	}
 
-        this.playerStateManager = playerStateManager;
+	private void publishNotification(final Notification notification, final PublicationType type) {
+		try {
+			boolean res = false;
+			if (type == PublicationType.EXTERNAL) {
+				if (externalPublisher != null) {
+					res = externalPublisher.publishNotification(notification);
+				} else {
+					return;
+				}
+			} else if (type == PublicationType.INTERNAL) {
+				if (internalPublisher != null) {
+					res = internalPublisher.publishNotification(notification);
+				} else {
+					return;
+				}
+			} else {
+				throw new UnsupportedOperationException("Publisher type is not supported: " + type);
+			}
 
-        if (this.playerStateManager != null) {
-            this.playerStateManager.addPlayerStateListener(stateListener);
-        }
-    }
-
-    public void setInternalPublisher(NotificationPublisher internalPublisher) {
-        this.internalPublisher = internalPublisher;
-    }
-
-    public void setExternalPublisher(NotificationPublisher externalPublisher) {
-        this.externalPublisher = externalPublisher;
-    }
-
-    public void setMandatoryNotifications(Set<String> mandatoryNotifications) {
-        if (this.mandatoryNotifications != null) {
-            this.mandatoryNotifications.clear();
-        }
-
-        if (mandatoryNotifications != null) {
-            this.mandatoryNotifications.addAll(mandatoryNotifications);
-        }
-    }
-
-    public void setRedundantNotifications(Set<String> redundantNotifications) {
-        if (this.redundantNotifications != null) {
-            this.redundantNotifications.clear();
-        }
-
-        if (redundantNotifications != null) {
-            this.redundantNotifications.addAll(redundantNotifications);
-        }
-    }
-
-    public void setNotificationManager(NotificationManager notificationManager) {
-        this.notificationManager = notificationManager;
-    }
+			if (res) {
+				for (NotificationDistributorListener listener : listeners) {
+					listener.notificationPublished(notification, type);
+				}
+			} else {
+				for (NotificationDistributorListener listener : listeners) {
+					listener.notificationRejected(notification, type);
+				}
+			}
+		} catch (Exception e) {
+			log.error("External notification can't be sent: " + notification, e);
+		}
+	}
 
 
-    private class ThePlayerStateListener implements PlayerStateListener {
-        private ThePlayerStateListener() {
-        }
+	public void setTaskExecutor(TaskExecutor taskExecutor) {
+		this.taskExecutor = taskExecutor;
+	}
 
-        @Override
-        public void playerOnline(Personality person) {
-            playerAlive(person);
-        }
+	public void setPlayerStateManager(PlayerStateManager playerStateManager) {
+		if (this.playerStateManager != null) {
+			this.playerStateManager.removePlayerStateListener(stateListener);
+		}
 
-        @Override
-        public void playerAlive(Personality person) {
-            lock.lock();
-            try {
-                waitingNotifications.remove(person);
-            } finally {
-                lock.unlock();
-            }
-        }
+		this.playerStateManager = playerStateManager;
 
-        @Override
-        public void playerOffline(Personality person) {
-            lock.lock();
-            try {
-                processUnpublishedNotifications(person);
-            } finally {
-                lock.unlock();
-            }
-        }
-    }
+		if (this.playerStateManager != null) {
+			this.playerStateManager.addPlayerStateListener(stateListener);
+		}
+	}
+
+	public void setInternalPublisher(NotificationPublisher internalPublisher) {
+		this.internalPublisher = internalPublisher;
+	}
+
+	public void setExternalPublisher(NotificationPublisher externalPublisher) {
+		this.externalPublisher = externalPublisher;
+	}
+
+	public void setMandatoryNotifications(Set<String> mandatoryNotifications) {
+		if (this.mandatoryNotifications != null) {
+			this.mandatoryNotifications.clear();
+		}
+
+		if (mandatoryNotifications != null) {
+			this.mandatoryNotifications.addAll(mandatoryNotifications);
+		}
+	}
+
+	public void setRedundantNotifications(Set<String> redundantNotifications) {
+		if (this.redundantNotifications != null) {
+			this.redundantNotifications.clear();
+		}
+
+		if (redundantNotifications != null) {
+			this.redundantNotifications.addAll(redundantNotifications);
+		}
+	}
+
+	public void setNotificationManager(NotificationManager notificationManager) {
+		this.notificationManager = notificationManager;
+	}
+
+
+	private class ThePlayerStateListener implements PlayerStateListener {
+		private ThePlayerStateListener() {
+		}
+
+		@Override
+		public void playerOnline(Personality person) {
+			playerAlive(person);
+		}
+
+		@Override
+		public void playerAlive(Personality person) {
+			lock.lock();
+			try {
+				waitingNotifications.remove(person);
+			} finally {
+				lock.unlock();
+			}
+		}
+
+		@Override
+		public void playerOffline(Personality person) {
+			lock.lock();
+			try {
+				processUnpublishedNotifications(person);
+			} finally {
+				lock.unlock();
+			}
+		}
+	}
 }
