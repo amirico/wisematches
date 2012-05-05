b08861d4c1515257cb9eb5df9448edd0cecf4c07
smklimenko <smklimenko@localhost>
2012-05-05 15:43:44 +0400

    + Issue 164:	Create game restrictions

--- modules/webserver/src/main/java/wisematches/server/web/controllers/playground/scribble/form/CreateScribbleForm.java
+++ modules/webserver/src/main/java/wisematches/server/web/controllers/playground/scribble/form/CreateScribbleForm.java
@@ -11,134 +11,152 @@ import javax.validation.constraints.NotNull;
  * @author Sergey Klimenko (smklimenko@gmail.com)
  */
 public class CreateScribbleForm {
-    @Length(max = 150, message = "game.create.title.err.max")
-    private String title;
+	@Length(max = 150, message = "game.create.title.err.max")
+	private String title;
 
-    @Min(value = 2, message = "game.create.time.err.min")
-    @Max(value = 14, message = "game.create.time.err.max")
-    private int daysPerMove = 3;
+	@Min(value = 2, message = "game.create.time.err.min")
+	@Max(value = 14, message = "game.create.time.err.max")
+	private int daysPerMove = 3;
 
-    private String boardLanguage;
+	private String boardLanguage;
 
-    @NotNull(message = "game.create.opponent.err.blank")
-    private CreateScribbleTab createTab = CreateScribbleTab.ROBOT;
+	@NotNull(message = "game.create.opponent.err.blank")
+	private CreateScribbleTab createTab = CreateScribbleTab.ROBOT;
 
-    private short minRating = 0;
-    private short maxRating = 0;
+	private int completed = 0;
+	private int timeouts = 0;
+	private short minRating = 0;
+	private short maxRating = 0;
 
-    private RobotType robotType = RobotType.TRAINEE;
+	private RobotType robotType = RobotType.TRAINEE;
 
-    @Min(value = 1, message = "game.create.opponents.err.min")
-    @Max(value = 3, message = "game.create.opponents.err.max")
-    private int opponentsCount = 1;
+	@Min(value = 1, message = "game.create.opponents.err.min")
+	@Max(value = 3, message = "game.create.opponents.err.max")
+	private int opponentsCount = 1;
 
-    private long[] opponents;
+	private long[] opponents;
 
-    @Length(max = 254, message = "game.create.opponent.challenge.err")
-    private String challengeMessage;
+	@Length(max = 254, message = "game.create.opponent.challenge.err")
+	private String challengeMessage;
 
-    private boolean commonError = false;
+	private boolean commonError = false;
 
-    private boolean rotten = false;
+	private boolean rotten = false;
 
-    public CreateScribbleForm() {
-    }
+	public CreateScribbleForm() {
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getBoardLanguage() {
-        return boardLanguage;
-    }
+	public String getBoardLanguage() {
+		return boardLanguage;
+	}
 
-    public void setBoardLanguage(String boardLanguage) {
-        this.boardLanguage = boardLanguage;
-    }
+	public void setBoardLanguage(String boardLanguage) {
+		this.boardLanguage = boardLanguage;
+	}
 
-    public int getDaysPerMove() {
-        return daysPerMove;
-    }
+	public int getDaysPerMove() {
+		return daysPerMove;
+	}
 
-    public void setDaysPerMove(int daysPerMove) {
-        this.daysPerMove = daysPerMove;
-    }
+	public void setDaysPerMove(int daysPerMove) {
+		this.daysPerMove = daysPerMove;
+	}
 
-    public CreateScribbleTab getCreateTab() {
-        return createTab;
-    }
+	public CreateScribbleTab getCreateTab() {
+		return createTab;
+	}
 
-    public void setCreateTab(CreateScribbleTab createTab) {
-        this.createTab = createTab;
-    }
+	public void setCreateTab(CreateScribbleTab createTab) {
+		this.createTab = createTab;
+	}
 
-    public RobotType getRobotType() {
-        return robotType;
-    }
+	public RobotType getRobotType() {
+		return robotType;
+	}
 
-    public void setRobotType(RobotType robotType) {
-        this.robotType = robotType;
-    }
+	public void setRobotType(RobotType robotType) {
+		this.robotType = robotType;
+	}
 
-    public short getMinRating() {
-        return minRating;
-    }
+	public short getMinRating() {
+		return minRating;
+	}
 
-    public void setMinRating(short minRating) {
-        this.minRating = minRating;
-    }
+	public void setMinRating(short minRating) {
+		this.minRating = minRating;
+	}
 
-    public short getMaxRating() {
-        return maxRating;
-    }
+	public short getMaxRating() {
+		return maxRating;
+	}
 
-    public void setMaxRating(short maxRating) {
-        this.maxRating = maxRating;
-    }
+	public void setMaxRating(short maxRating) {
+		this.maxRating = maxRating;
+	}
 
-    public int getOpponentsCount() {
-        return opponentsCount;
-    }
+	public int getCompleted() {
+		return completed;
+	}
 
-    public void setOpponentsCount(int opponentsCount) {
-        this.opponentsCount = opponentsCount;
-    }
+	public void setCompleted(int completed) {
+		this.completed = completed;
+	}
 
-    public long[] getOpponents() {
-        return opponents;
-    }
+	public int getTimeouts() {
+		return timeouts;
+	}
 
-    public void setOpponents(long[] opponents) {
-        this.opponents = opponents;
-    }
+	public void setTimeouts(int timeouts) {
+		this.timeouts = timeouts;
+	}
 
-    public String getChallengeMessage() {
-        return challengeMessage;
-    }
+	public int getOpponentsCount() {
+		return opponentsCount;
+	}
 
-    public void setChallengeMessage(String challengeMessage) {
-        this.challengeMessage = challengeMessage;
-    }
+	public void setOpponentsCount(int opponentsCount) {
+		this.opponentsCount = opponentsCount;
+	}
 
-    public boolean isRotten() {
-        return rotten;
-    }
+	public long[] getOpponents() {
+		return opponents;
+	}
 
-    public void setRotten(boolean rotten) {
-        this.rotten = rotten;
-    }
+	public void setOpponents(long[] opponents) {
+		this.opponents = opponents;
+	}
 
-    public boolean isCommonError() {
-        return commonError;
-    }
+	public String getChallengeMessage() {
+		return challengeMessage;
+	}
 
-    public void setCommonError(boolean commonError) {
-        this.commonError = commonError;
-    }
+	public void setChallengeMessage(String challengeMessage) {
+		this.challengeMessage = challengeMessage;
+	}
+
+	public boolean isRotten() {
+		return rotten;
+	}
+
+	public void setRotten(boolean rotten) {
+		this.rotten = rotten;
+	}
+
+	public boolean isCommonError() {
+		return commonError;
+	}
+
+	public void setCommonError(boolean commonError) {
+		this.commonError = commonError;
+	}
 
 	@Override
 	public String toString() {
