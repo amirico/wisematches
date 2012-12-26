caeedbc8e5efdde1acb2dd0c2fbd3757532d3c0e
smklimenko <smklimenko@localhost>
2012-12-26 22:45:18 +0400

    ~ Issue 249:	Dictionary management

--- modules/webserver/src/main/java/wisematches/server/web/controllers/playground/vocabulary/VocabularyController.java
+++ modules/webserver/src/main/java/wisematches/server/web/controllers/playground/vocabulary/VocabularyController.java
@@ -5,13 +5,18 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import wisematches.personality.Language;
-import wisematches.playground.dictionary.Dictionary;
 import wisematches.playground.dictionary.DictionaryManager;
 import wisematches.playground.dictionary.DictionaryNotFoundException;
+import wisematches.playground.dictionary.IterableDictionary;
 import wisematches.server.web.controllers.UnknownEntityException;
 import wisematches.server.web.controllers.WisematchesController;
+import wisematches.server.web.controllers.playground.vocabulary.view.VocabularyDistribution;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Locale;
 
 /**
@@ -25,17 +30,42 @@ public class VocabularyController extends WisematchesController {
     public VocabularyController() {
     }
 
-    @RequestMapping("view")
-    public String showReceivedMessage(Model model) throws UnknownEntityException {
-        final Locale locale = Language.RU.locale();
+    @RequestMapping("")
+    public String showReceivedMessage(@RequestParam("p") String prefix, Model model) throws UnknownEntityException {
+        final Language language = Language.RU;
+        final Locale locale = language.locale();
         try {
-            final Dictionary dictionary = dictionaryManager.getDictionary(locale);
-            model.addAttribute("dictionary", dictionary);
+            final IterableDictionary dictionary = (IterableDictionary) dictionaryManager.getDictionary(locale);
+            model.addAttribute("words", loadWords("ru", prefix, model));
+            model.addAttribute("distribution", new VocabularyDistribution(dictionary));
             return "/content/playground/vocabulary/view";
         } catch (DictionaryNotFoundException ex) {
-            throw new UnknownEntityException(locale, "dictionary");
+            throw new UnknownEntityException(language, "dictionary");
         }
     }
+
+    @ResponseBody
+    @RequestMapping("load.ajax")
+    public Collection<String> loadWords(
+            @RequestParam("l") String lang, @RequestParam("p") String prefix, Model model) throws UnknownEntityException {
+        final Language language = Language.byCode(lang.toUpperCase());
+
+
+        try {
+            prefix = prefix.toLowerCase();
+            final IterableDictionary dictionary = (IterableDictionary) dictionaryManager.getDictionary(language.locale());
+            Collection<String> strings = new ArrayList<>();
+            for (String word : dictionary) {
+                if (word.startsWith(prefix)) {
+                    strings.add(word);
+                }
+            }
+            return strings;
+        } catch (DictionaryNotFoundException ex) {
+            throw new UnknownEntityException(language, "dictionary");
+        }
+    }
+
 
     @Autowired
     public void setDictionaryManager(@Qualifier("lexicalDictionary") DictionaryManager dictionaryManager) {
