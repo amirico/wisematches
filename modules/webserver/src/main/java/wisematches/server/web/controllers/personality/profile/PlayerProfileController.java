8582db57ab2f04878ca483d5cd0a2454d510e71e
smklimenko <smklimenko@localhost>
2013-01-11 23:34:57 +0400

    release 4.1.0: Awards are released

--- modules/webserver/src/main/java/wisematches/server/web/controllers/personality/profile/PlayerProfileController.java
+++ modules/webserver/src/main/java/wisematches/server/web/controllers/personality/profile/PlayerProfileController.java
@@ -47,234 +47,234 @@ import java.util.Locale;
 @Controller
 @RequestMapping("/playground/profile")
 public class PlayerProfileController extends WisematchesController {
-	private PlayerManager playerManager;
-	private AwardsManager awardsManager;
-	private GameMessageSource messageSource;
-	private CountriesManager countriesManager;
-	private StatisticManager statisticManager;
-	private PlayerProfileManager profileManager;
-	private BoardSettingsManager boardSettingsManager;
+    private PlayerManager playerManager;
+    private AwardsManager awardsManager;
+    private GameMessageSource messageSource;
+    private CountriesManager countriesManager;
+    private StatisticManager statisticManager;
+    private PlayerProfileManager profileManager;
+    private BoardSettingsManager boardSettingsManager;
 
-	private static final ThreadLocal<Calendar> CALENDAR_THREAD_LOCAL = new ThreadLocal<Calendar>() {
-		@Override
-		protected Calendar initialValue() {
-			return Calendar.getInstance();
-		}
-	};
+    private static final ThreadLocal<Calendar> CALENDAR_THREAD_LOCAL = new ThreadLocal<Calendar>() {
+        @Override
+        protected Calendar initialValue() {
+            return Calendar.getInstance();
+        }
+    };
 
-	private static final ThreadLocal<DateFormat> DATE_FORMAT_THREAD_LOCAL = new ThreadLocal<DateFormat>() {
-		@Override
-		protected DateFormat initialValue() {
-			return new SimpleDateFormat("dd-MM-yyyy");
-		}
-	};
+    private static final ThreadLocal<DateFormat> DATE_FORMAT_THREAD_LOCAL = new ThreadLocal<DateFormat>() {
+        @Override
+        protected DateFormat initialValue() {
+            return new SimpleDateFormat("dd-MM-yyyy");
+        }
+    };
 
-	private static final TourneyPlace[] TOURNEY_PLACEs = TourneyPlace.values();
+    private static final TourneyPlace[] TOURNEY_PLACEs = TourneyPlace.values();
 
-	public PlayerProfileController() {
-	}
+    public PlayerProfileController() {
+    }
 
-	@RequestMapping("view")
-	public String viewProfile(@RequestParam(value = "p", required = false) String profileId, Model model, Locale locale) throws UnknownEntityException {
-		try {
-			Player player;
-			try {
-				player = playerManager.getPlayer(Long.parseLong(profileId));
-			} catch (NumberFormatException ignore) {
-				player = null;
-			}
-			if (player == null) {
-				throw new UnknownEntityException(profileId, "profile");
-			}
+    @RequestMapping("view")
+    public String viewProfile(@RequestParam(value = "p", required = false) String profileId, Model model, Locale locale) throws UnknownEntityException {
+        try {
+            Player player;
+            try {
+                player = playerManager.getPlayer(Long.parseLong(profileId));
+            } catch (NumberFormatException ignore) {
+                player = null;
+            }
+            if (player == null) {
+                throw new UnknownEntityException(profileId, "profile");
+            }
 
-			final Calendar c = CALENDAR_THREAD_LOCAL.get();
-			c.setTimeInMillis(System.currentTimeMillis());
-			c.set(Calendar.MILLISECOND, 0);
-			c.set(Calendar.SECOND, 0);
-			c.set(Calendar.MINUTE, 0);
-			c.set(Calendar.HOUR, 0);
-			c.set(Calendar.DAY_OF_MONTH, 1);
-			c.add(Calendar.MONTH, 1);
+            final Calendar c = CALENDAR_THREAD_LOCAL.get();
+            c.setTimeInMillis(System.currentTimeMillis());
+            c.set(Calendar.MILLISECOND, 0);
+            c.set(Calendar.SECOND, 0);
+            c.set(Calendar.MINUTE, 0);
+            c.set(Calendar.HOUR, 0);
+            c.set(Calendar.DAY_OF_MONTH, 1);
+            c.add(Calendar.MONTH, 1);
 
-			final int middle = c.get(Calendar.MONTH);
-			final Date end = c.getTime();
+            final int middle = c.get(Calendar.MONTH);
+            final Date end = c.getTime();
 
-			c.add(Calendar.DAY_OF_YEAR, -365);
-			final Date start = c.getTime();
+            c.add(Calendar.DAY_OF_YEAR, -365);
+            final Date start = c.getTime();
 
-			final PlayerProfile profile = profileManager.getPlayerProfile(player);
-			final Statistics statistics = statisticManager.getPlayerStatistic(player);
-			final RatingCurve ratingCurve = statisticManager.getRatingCurve(player, 10, start, end);
-			final BoardSettings boardSettings = boardSettingsManager.getScribbleSettings(getPersonality());
+            final PlayerProfile profile = profileManager.getPlayerProfile(player);
+            final Statistics statistics = statisticManager.getPlayerStatistic(player);
+            final RatingCurve ratingCurve = statisticManager.getRatingCurve(player, 10, start, end);
+            final BoardSettings boardSettings = boardSettingsManager.getScribbleSettings(getPersonality());
 
-			final RatingChart ratingChart = new RatingChart(ratingCurve, middle);
+            final RatingChart ratingChart = new RatingChart(ratingCurve, middle);
 
-			if (profile.getCountryCode() != null) {
-				model.addAttribute("country", countriesManager.getCountry(profile.getCountryCode(), Language.byLocale(locale)));
-			}
+            if (profile.getCountryCode() != null) {
+                model.addAttribute("country", countriesManager.getCountry(profile.getCountryCode(), Language.byLocale(locale)));
+            }
 
-			model.addAttribute("player", player);
-			model.addAttribute("profile", profile);
-			model.addAttribute("statistics", statistics);
-			model.addAttribute("ratingChart", ratingChart);
-			model.addAttribute("tourneyMedals", TOURNEY_PLACEs);
-			model.addAttribute("boardSettings", boardSettings);
+            model.addAttribute("player", player);
+            model.addAttribute("profile", profile);
+            model.addAttribute("statistics", statistics);
+            model.addAttribute("ratingChart", ratingChart);
+            model.addAttribute("tourneyMedals", TOURNEY_PLACEs);
+            model.addAttribute("boardSettings", boardSettings);
 
-			model.addAttribute("awardsSummary", awardsManager.getAwardsSummary(player));
+            model.addAttribute("awardsSummary", awardsManager.getAwardsSummary(player));
 
-			return "/content/playground/profile/view";
-		} catch (NumberFormatException ex) {
-			throw new UnknownEntityException(profileId, "profile");
-		}
-	}
+            return "/content/playground/profile/view";
+        } catch (NumberFormatException ex) {
+            throw new UnknownEntityException(profileId, "profile");
+        }
+    }
 
-	@RequestMapping("awards")
-	public String viewAwards(@RequestParam(value = "p", required = false) String profileId, Model model, Locale locale) throws UnknownEntityException {
-		try {
-			Player player = playerManager.getPlayer(Long.parseLong(profileId));
-			if (player == null) {
-				throw new UnknownEntityException(profileId, "profile");
-			}
+    @RequestMapping("awards")
+    public String viewAwards(@RequestParam(value = "p", required = false) String profileId, Model model, Locale locale) throws UnknownEntityException {
+        try {
+            Player player = playerManager.getPlayer(Long.parseLong(profileId));
+            if (player == null) {
+                throw new UnknownEntityException(profileId, "profile");
+            }
 
-			final PlayerProfile profile = profileManager.getPlayerProfile(player);
-			if (profile.getCountryCode() != null) {
-				model.addAttribute("country", countriesManager.getCountry(profile.getCountryCode(), Language.byLocale(locale)));
-			}
+            final PlayerProfile profile = profileManager.getPlayerProfile(player);
+            if (profile.getCountryCode() != null) {
+                model.addAttribute("country", countriesManager.getCountry(profile.getCountryCode(), Language.byLocale(locale)));
+            }
 
-			final List<Award> awards = awardsManager.searchEntities(getPersonality(), new AwardContext(null, null), null, Orders.of(Order.desc("awardedDate")), null);
-			model.addAttribute("player", player);
-			model.addAttribute("profile", profile);
-			model.addAttribute("awards", awards);
-			return "/content/playground/profile/awards";
-		} catch (NumberFormatException ex) {
-			throw new UnknownEntityException(profileId, "profile");
-		}
-	}
+            final List<Award> awards = awardsManager.searchEntities(player, new AwardContext(null, null), null, Orders.of(Order.desc("awardedDate")), null);
+            model.addAttribute("player", player);
+            model.addAttribute("profile", profile);
+            model.addAttribute("awards", awards);
+            return "/content/playground/profile/awards";
+        } catch (NumberFormatException ex) {
+            throw new UnknownEntityException(profileId, "profile");
+        }
+    }
 
-	@RequestMapping("edit")
-	public String editProfile(Model model, Locale locale) {
-		final Player principal = getPrincipal();
-		final PlayerProfile profile = profileManager.getPlayerProfile(principal);
+    @RequestMapping("edit")
+    public String editProfile(Model model, Locale locale) {
+        final Player principal = getPrincipal();
+        final PlayerProfile profile = profileManager.getPlayerProfile(principal);
 
-		final DateFormat dateFormat = DATE_FORMAT_THREAD_LOCAL.get();
+        final DateFormat dateFormat = DATE_FORMAT_THREAD_LOCAL.get();
 
-		final PlayerProfileForm form = new PlayerProfileForm();
-		form.setComments(profile.getComments());
-		form.setCountryCode(profile.getCountryCode());
+        final PlayerProfileForm form = new PlayerProfileForm();
+        form.setComments(profile.getComments());
+        form.setCountryCode(profile.getCountryCode());
 
-		form.setRealName(profile.getRealName());
+        form.setRealName(profile.getRealName());
 
-		if (profile.getBirthday() != null) {
-			form.setBirthday(dateFormat.format(profile.getBirthday()));
-		}
-		if (profile.getGender() != null) {
-			form.setGender(profile.getGender().name().toLowerCase());
-		}
-		if (profile.getPrimaryLanguage() != null) {
-			form.setPrimaryLanguage(profile.getPrimaryLanguage().code().toLowerCase());
-		}
-		if (profile.getCountryCode() != null) {
-			final Country country = countriesManager.getCountry(profile.getCountryCode(), Language.byLocale(locale));
-			if (country != null) {
-				form.setCountry(country.getName());
-			}
-		}
+        if (profile.getBirthday() != null) {
+            form.setBirthday(dateFormat.format(profile.getBirthday()));
+        }
+        if (profile.getGender() != null) {
+            form.setGender(profile.getGender().name().toLowerCase());
+        }
+        if (profile.getPrimaryLanguage() != null) {
+            form.setPrimaryLanguage(profile.getPrimaryLanguage().code().toLowerCase());
+        }
+        if (profile.getCountryCode() != null) {
+            final Country country = countriesManager.getCountry(profile.getCountryCode(), Language.byLocale(locale));
+            if (country != null) {
+                form.setCountry(country.getName());
+            }
+        }
 
-		model.addAttribute("player", principal);
-		model.addAttribute("profile", profile);
-		model.addAttribute("profileForm", form);
-		model.addAttribute("countries", countriesManager.getCountries(Language.byLocale(locale)));
-		return "/content/playground/profile/edit";
-	}
+        model.addAttribute("player", principal);
+        model.addAttribute("profile", profile);
+        model.addAttribute("profileForm", form);
+        model.addAttribute("countries", countriesManager.getCountries(Language.byLocale(locale)));
+        return "/content/playground/profile/edit";
+    }
 
-	@ResponseBody
-	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	@RequestMapping(value = "save", method = RequestMethod.POST)
-	public ServiceResponse saveProfile(@RequestBody final PlayerProfileForm form, Locale locale) {
-		try {
-			final DateFormat dateFormat = DATE_FORMAT_THREAD_LOCAL.get();
+    @ResponseBody
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public ServiceResponse saveProfile(@RequestBody final PlayerProfileForm form, Locale locale) {
+        try {
+            final DateFormat dateFormat = DATE_FORMAT_THREAD_LOCAL.get();
 
-			final PlayerProfile profile = profileManager.getPlayerProfile(getPersonality());
+            final PlayerProfile profile = profileManager.getPlayerProfile(getPersonality());
 
-			final PlayerProfileEditor editor = new PlayerProfileEditor(profile);
-			editor.setRealName(form.getRealName());
-			editor.setComments(form.getComments());
-			editor.setCountryCode(form.getCountryCode());
+            final PlayerProfileEditor editor = new PlayerProfileEditor(profile);
+            editor.setRealName(form.getRealName());
+            editor.setComments(form.getComments());
+            editor.setCountryCode(form.getCountryCode());
 
-			if (form.getBirthday() == null || form.getBirthday().trim().length() == 0) {
-				editor.setBirthday(null);
-			} else {
-				try {
-					editor.setBirthday(dateFormat.parse(form.getBirthday()));
-				} catch (ParseException ex) {
-					return ServiceResponse.failure(messageSource.getMessage("profile.edit.error.birthday", locale));
-				}
-			}
+            if (form.getBirthday() == null || form.getBirthday().trim().length() == 0) {
+                editor.setBirthday(null);
+            } else {
+                try {
+                    editor.setBirthday(dateFormat.parse(form.getBirthday()));
+                } catch (ParseException ex) {
+                    return ServiceResponse.failure(messageSource.getMessage("profile.edit.error.birthday", locale));
+                }
+            }
 
-			if (form.getGender() == null || form.getGender().trim().length() == 0) {
-				editor.setGender(null);
-			} else {
-				try {
-					editor.setGender(Gender.valueOf(form.getGender().toUpperCase()));
-				} catch (IllegalArgumentException ex) {
-					return ServiceResponse.failure(messageSource.getMessage("profile.edit.error.gender", locale));
-				}
-			}
+            if (form.getGender() == null || form.getGender().trim().length() == 0) {
+                editor.setGender(null);
+            } else {
+                try {
+                    editor.setGender(Gender.valueOf(form.getGender().toUpperCase()));
+                } catch (IllegalArgumentException ex) {
+                    return ServiceResponse.failure(messageSource.getMessage("profile.edit.error.gender", locale));
+                }
+            }
 
-			if (form.getPrimaryLanguage() == null || form.getPrimaryLanguage().trim().length() == 0) {
-				editor.setPrimaryLanguage(null);
-			} else {
-				try {
-					editor.setPrimaryLanguage(Language.valueOf(form.getPrimaryLanguage().toUpperCase()));
-				} catch (IllegalArgumentException ex) {
-					return ServiceResponse.failure(messageSource.getMessage("profile.edit.error.primary", locale));
-				}
-			}
-			profileManager.updateProfile(editor.createProfile());
+            if (form.getPrimaryLanguage() == null || form.getPrimaryLanguage().trim().length() == 0) {
+                editor.setPrimaryLanguage(null);
+            } else {
+                try {
+                    editor.setPrimaryLanguage(Language.valueOf(form.getPrimaryLanguage().toUpperCase()));
+                } catch (IllegalArgumentException ex) {
+                    return ServiceResponse.failure(messageSource.getMessage("profile.edit.error.primary", locale));
+                }
+            }
+            profileManager.updateProfile(editor.createProfile());
 
-			return ServiceResponse.SUCCESS;
-		} catch (Exception ex) {
-			return ServiceResponse.failure(messageSource.getMessage("profile.edit.error.system", locale, ex.getMessage()));
-		}
-	}
+            return ServiceResponse.SUCCESS;
+        } catch (Exception ex) {
+            return ServiceResponse.failure(messageSource.getMessage("profile.edit.error.system", locale, ex.getMessage()));
+        }
+    }
 
-	@Autowired
-	public void setPlayerManager(PlayerManager playerManager) {
-		this.playerManager = playerManager;
-	}
+    @Autowired
+    public void setPlayerManager(PlayerManager playerManager) {
+        this.playerManager = playerManager;
+    }
 
-	@Autowired
-	public void setAwardsManager(AwardsManager awardsManager) {
-		this.awardsManager = awardsManager;
-	}
+    @Autowired
+    public void setAwardsManager(AwardsManager awardsManager) {
+        this.awardsManager = awardsManager;
+    }
 
-	@Autowired
-	public void setProfileManager(PlayerProfileManager profileManager) {
-		this.profileManager = profileManager;
-	}
+    @Autowired
+    public void setProfileManager(PlayerProfileManager profileManager) {
+        this.profileManager = profileManager;
+    }
 
-	@Autowired
-	public void setCountriesManager(CountriesManager countriesManager) {
-		this.countriesManager = countriesManager;
-	}
+    @Autowired
+    public void setCountriesManager(CountriesManager countriesManager) {
+        this.countriesManager = countriesManager;
+    }
 
-	@Autowired
-	public void setStatisticManager(StatisticManager statisticManager) {
-		this.statisticManager = statisticManager;
-	}
+    @Autowired
+    public void setStatisticManager(StatisticManager statisticManager) {
+        this.statisticManager = statisticManager;
+    }
 
-	@Autowired
-	public void setGameMessageSource(GameMessageSource gameMessageSource) {
-		this.messageSource = gameMessageSource;
-	}
+    @Autowired
+    public void setGameMessageSource(GameMessageSource gameMessageSource) {
+        this.messageSource = gameMessageSource;
+    }
 
-	@Autowired
-	public void setBoardSettingsManager(BoardSettingsManager boardSettingsManager) {
-		this.boardSettingsManager = boardSettingsManager;
-	}
+    @Autowired
+    public void setBoardSettingsManager(BoardSettingsManager boardSettingsManager) {
+        this.boardSettingsManager = boardSettingsManager;
+    }
 
-	@ModelAttribute("headerTitle")
-	public String getHeaderTitle() {
-		return "title.profile";
-	}
+    @ModelAttribute("headerTitle")
+    public String getHeaderTitle() {
+        return "title.profile";
+    }
 }
