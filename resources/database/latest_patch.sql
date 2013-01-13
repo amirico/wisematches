0c8b2351fe4e81ade66b24722d4719e31c9b745e
smklimenko <smklimenko@localhost>
2013-01-13 09:58:40 +0400

    + Issue 287:	???? ????? ???????? + Issue 288:	New player can't be registered + Issue 289:	Robot awards don't work + Issue 290:	Google tracker doesn't work

--- resources/database/latest_patch.sql
+++ resources/database/latest_patch.sql
@@ -34,3 +34,69 @@ WHERE p.playerId > 100 AND r.playerId < 100 AND p.points > r.points AND p.newRat
       AND r.playerId = 3
 GROUP BY p.playerId, r.playerId
 HAVING count(*) >= 5;
+
+UPDATE scribble_statistic AS s LEFT JOIN (SELECT
+                                            p.playerId AS pid,
+                                            count(*)   AS cnt
+                                          FROM
+                                              scribble_player p
+                                              LEFT JOIN
+                                              scribble_player r
+                                                ON p.boardId = r.boardId AND p.playerId != r.playerId
+                                              LEFT JOIN
+                                              scribble_board b
+                                                ON b.boardId = p.boardId
+                                          WHERE p.playerId > 100 AND r.playerId < 100 AND p.points > r.points AND
+                                                p.newRating > p.oldRating
+                                                AND r.playerId = 2
+                                          GROUP BY p.playerId) AS w
+    ON pid = s.playerId
+SET winsRD = cnt;
+
+UPDATE scribble_statistic AS s LEFT JOIN (SELECT
+                                            p.playerId AS pid,
+                                            count(*)   AS cnt
+                                          FROM
+                                              scribble_player p
+                                              LEFT JOIN
+                                              scribble_player r
+                                                ON p.boardId = r.boardId AND p.playerId != r.playerId
+                                              LEFT JOIN
+                                              scribble_board b
+                                                ON b.boardId = p.boardId
+                                          WHERE p.playerId > 100 AND r.playerId < 100 AND p.points > r.points AND
+                                                p.newRating > p.oldRating
+                                                AND r.playerId = 3
+                                          GROUP BY p.playerId) AS w
+    ON pid = s.playerId
+SET winsRT = cnt;
+
+UPDATE scribble_statistic AS s LEFT JOIN (SELECT
+                                            p.playerId AS pid,
+                                            count(*)   AS cnt
+                                          FROM
+                                              scribble_player p
+                                              LEFT JOIN
+                                              scribble_player r
+                                                ON p.boardId = r.boardId AND p.playerId != r.playerId
+                                              LEFT JOIN
+                                              scribble_board b
+                                                ON b.boardId = p.boardId
+                                          WHERE p.playerId > 100 AND r.playerId < 100 AND p.points > r.points AND
+                                                p.newRating > p.oldRating
+                                                AND r.playerId = 4
+                                          GROUP BY p.playerId) AS w
+    ON pid = s.playerId
+SET winsRE = cnt;
+
+UPDATE scribble_statistic
+SET winsRE = 0
+WHERE winsRE IS null;
+UPDATE scribble_statistic
+SET winsRT = 0
+WHERE winsRT IS null;
+UPDATE scribble_statistic
+SET winsRD = 0
+WHERE winsRD IS null;
+
+
