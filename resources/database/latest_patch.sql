7e772a129d59a3a2702725cfc83920751a299642
smklimenko <smklimenko@localhost>
2013-01-14 21:15:43 +0400

    4.1.3-SNAPSHOT started

--- resources/database/latest_patch.sql
+++ resources/database/latest_patch.sql
@@ -1,5 +1,2 @@
-ALTER TABLE `wisematches`.`notification_settings` CHANGE COLUMN `playground.tourney.finished` `playground.award.granted` TINYINT(4) NULL DEFAULT NULL;
-
-UPDATE notification_settings
-SET `playground.award.granted` = 0
-WHERE `playground.award.granted` = 2;
+ALTER TABLE `wisematches`.`notification_settings` ADD COLUMN `playground.award.granted` TINYINT(4) NULL DEFAULT 0
+AFTER `playground.dictionary.rejected`;
