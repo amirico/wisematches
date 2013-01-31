6e5ed7658c3662e950978a384b3a98a22619de7f
smklimenko <smklimenko@localhost>
2013-01-31 13:42:52 +0400

    ! 5.0.0 is started: some tests for server package fixed

--- modules/server/src/test/java/wisematches/server/services/friends/impl/HibernateFriendsManagerTest.java
+++ modules/server/src/test/java/wisematches/server/services/friends/impl/HibernateFriendsManagerTest.java
@@ -2,9 +2,20 @@ package wisematches.server.services.friends.impl;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import wisematches.core.Player;
+import wisematches.server.services.MockPlayer;
+import wisematches.server.services.friends.FriendRelation;
+import wisematches.server.services.friends.FriendsListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Sergey Klimenko (smklimenko@gmail.com)
@@ -13,16 +24,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
 		"classpath:/config/database-junit-config.xml",
-		"classpath:/config/accounts-config.xml",
+		"classpath:/config/personality-config.xml",
+		"classpath:/config/scribble-config.xml",
 		"classpath:/config/playground-junit-config.xml"
 })
 public class HibernateFriendsManagerTest {
-	@Test
-	public void commented() {
-		throw new UnsupportedOperationException("commented");
-	}
-
-/*
 	@Autowired
 	private HibernateFriendsManager friendsManager;
 
@@ -31,60 +37,63 @@ public class HibernateFriendsManagerTest {
 
 	@Test
 	public void test() {
+		final Player p1 = new MockPlayer(1);
+		final Player p2 = new MockPlayer(2);
+		final Player p3 = new MockPlayer(3);
+
 		final FriendsListener listener = createStrictMock(FriendsListener.class);
-		listener.friendAdded(same(RobotPlayer.DULL), same(RobotPlayer.EXPERT), isA(FriendRelation.class));
-		listener.friendAdded(same(RobotPlayer.DULL), same(RobotPlayer.TRAINEE), isA(FriendRelation.class));
-		listener.friendAdded(same(RobotPlayer.EXPERT), same(RobotPlayer.TRAINEE), isA(FriendRelation.class));
-		listener.friendRemoved(same(RobotPlayer.DULL), same(RobotPlayer.EXPERT), isA(FriendRelation.class));
-		listener.friendRemoved(same(RobotPlayer.EXPERT), same(RobotPlayer.TRAINEE), isA(FriendRelation.class));
-		listener.friendRemoved(same(RobotPlayer.DULL), same(RobotPlayer.TRAINEE), isA(FriendRelation.class));
+		listener.friendAdded(same(p1), same(p3), isA(FriendRelation.class));
+		listener.friendAdded(same(p1), same(p2), isA(FriendRelation.class));
+		listener.friendAdded(same(p3), same(p2), isA(FriendRelation.class));
+		listener.friendRemoved(same(p1), same(p3), isA(FriendRelation.class));
+		listener.friendRemoved(same(p3), same(p2), isA(FriendRelation.class));
+		listener.friendRemoved(same(p1), same(p2), isA(FriendRelation.class));
 		replay(listener);
 
 		friendsManager.addFriendsListener(listener);
 
-		friendsManager.addFriend(RobotPlayer.DULL, RobotPlayer.EXPERT, "asd");
-		friendsManager.addFriend(RobotPlayer.DULL, RobotPlayer.TRAINEE, "qwe");
-		friendsManager.addFriend(RobotPlayer.EXPERT, RobotPlayer.TRAINEE, "zxc");
+		friendsManager.addFriend(p1, p3, "asd");
+		friendsManager.addFriend(p1, p2, "qwe");
+		friendsManager.addFriend(p3, p2, "zxc");
 
-		final List<FriendRelation> fl1 = new ArrayList<FriendRelation>(friendsManager.getFriendsList(RobotPlayer.DULL));
+		final List<FriendRelation> fl1 = new ArrayList<FriendRelation>(friendsManager.getFriendsList(p1));
 		assertEquals(2, fl1.size());
-		assertEquals(2, friendsManager.getFriendsIds(RobotPlayer.DULL).size());
-		assertRelation(fl1.get(0), RobotPlayer.DULL, RobotPlayer.TRAINEE, "qwe");
-		assertRelation(fl1.get(1), RobotPlayer.DULL, RobotPlayer.EXPERT, "asd");
+		assertEquals(2, friendsManager.getFriendsIds(p1).size());
+		assertRelation(fl1.get(0), p1, p2, "qwe");
+		assertRelation(fl1.get(1), p1, p3, "asd");
 
-		final List<FriendRelation> fl2 = new ArrayList<FriendRelation>(friendsManager.getFriendsList(RobotPlayer.EXPERT));
+		final List<FriendRelation> fl2 = new ArrayList<FriendRelation>(friendsManager.getFriendsList(p3));
 		assertEquals(1, fl2.size());
-		assertEquals(1, friendsManager.getFriendsIds(RobotPlayer.EXPERT).size());
-		assertRelation(fl2.get(0), RobotPlayer.EXPERT, RobotPlayer.TRAINEE, "zxc");
+		assertEquals(1, friendsManager.getFriendsIds(p3).size());
+		assertRelation(fl2.get(0), p3, p2, "zxc");
 
-		final List<FriendRelation> fl3 = new ArrayList<FriendRelation>(friendsManager.getFriendsList(RobotPlayer.TRAINEE));
+		final List<FriendRelation> fl3 = new ArrayList<FriendRelation>(friendsManager.getFriendsList(p2));
 		assertEquals(0, fl3.size());
-		assertEquals(0, friendsManager.getFriendsIds(RobotPlayer.TRAINEE).size());
+		assertEquals(0, friendsManager.getFriendsIds(p2).size());
 
-		friendsManager.removeFriend(RobotPlayer.DULL, RobotPlayer.EXPERT);
-		assertEquals(1, friendsManager.getFriendsList(RobotPlayer.DULL).size());
+		friendsManager.removeFriend(p1, p3);
+		assertEquals(1, friendsManager.getFriendsList(p1).size());
 
-		friendsManager.removeFriend(RobotPlayer.EXPERT, RobotPlayer.TRAINEE);
-		assertEquals(0, friendsManager.getFriendsList(RobotPlayer.EXPERT).size());
+		friendsManager.removeFriend(p3, p2);
+		assertEquals(0, friendsManager.getFriendsList(p3).size());
 
-		friendsManager.removeFriend(RobotPlayer.DULL, RobotPlayer.DULL);
-		assertEquals(1, friendsManager.getFriendsList(RobotPlayer.DULL).size());
+		friendsManager.removeFriend(p1, p1);
+		assertEquals(1, friendsManager.getFriendsList(p1).size());
 
-		friendsManager.addFriend(RobotPlayer.DULL, RobotPlayer.TRAINEE, "hdfgh");
-		final List<FriendRelation> fl4 = new ArrayList<FriendRelation>(friendsManager.getFriendsList(RobotPlayer.DULL));
+		friendsManager.addFriend(p1, p2, "hdfgh");
+		final List<FriendRelation> fl4 = new ArrayList<FriendRelation>(friendsManager.getFriendsList(p1));
 		assertEquals(1, fl4.size());
-		assertRelation(fl1.get(0), RobotPlayer.DULL, RobotPlayer.TRAINEE, "hdfgh");
+		assertRelation(fl1.get(0), p1, p2, "hdfgh");
 
-		friendsManager.removeFriend(RobotPlayer.DULL, RobotPlayer.TRAINEE);
-		assertEquals(0, friendsManager.getFriendsList(RobotPlayer.DULL).size());
+		friendsManager.removeFriend(p1, p2);
+		assertEquals(0, friendsManager.getFriendsList(p1).size());
 
 		verify(listener);
 	}
 
-	private void assertRelation(FriendRelation f1, RobotPlayer person, RobotPlayer friend, String comment) {
-		assertEquals(person.getId(), f1.getPerson());
-		assertEquals(friend.getId(), f1.getFriend());
+	private void assertRelation(FriendRelation f1, Player person, Player friend, String comment) {
+		assertEquals(person.getId().longValue(), f1.getPerson());
+		assertEquals(friend.getId().longValue(), f1.getFriend());
 		assertEquals(comment, f1.getComment());
 	}
-*/
 }
--- modules/server/src/test/java/wisematches/server/services/message/impl/HibernateMessageManagerTest.java
+++ modules/server/src/test/java/wisematches/server/services/message/impl/HibernateMessageManagerTest.java
@@ -1,10 +1,30 @@
 package wisematches.server.services.message.impl;
 
+import org.easymock.EasyMock;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import wisematches.core.Language;
+import wisematches.core.Player;
+import wisematches.core.personality.player.Guest;
+import wisematches.core.personality.player.Membership;
+import wisematches.playground.restriction.impl.RestrictionDescription;
+import wisematches.playground.restriction.impl.RestrictionManagerImpl;
+import wisematches.server.services.message.Message;
+import wisematches.server.services.message.MessageDirection;
+import wisematches.server.services.message.MessageManager;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Sergey Klimenko (smklimenko@gmail.com)
@@ -13,15 +33,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
 		"classpath:/config/database-junit-config.xml",
-		"classpath:/config/accounts-config.xml",
+		"classpath:/config/personality-config.xml",
+		"classpath:/config/scribble-config.xml",
 		"classpath:/config/playground-junit-config.xml"
 })
 public class HibernateMessageManagerTest {
-	@Test
-	public void commented() {
-		throw new UnsupportedOperationException("commented");
-	}
-/*
 	@Autowired
 	MessageManager messageManager;
 
@@ -30,85 +46,85 @@ public class HibernateMessageManagerTest {
 
 	@Test
 	public void testMessage() {
-		final Personality p1 = Personality.person(998);
-		final Personality p2 = Personality.person(999);
+		final Player p1 = Guest.byLanguage(Language.RU);
+		final Player p2 = Guest.byLanguage(Language.EN);
 
-		Assert.assertEquals(0, messageManager.getTodayMessagesCount(p1, MessageDirection.RECEIVED));
-		Assert.assertEquals(0, messageManager.getTodayMessagesCount(p1, MessageDirection.SENT));
-		Assert.assertEquals(0, messageManager.getTodayMessagesCount(p2, MessageDirection.SENT));
-		Assert.assertEquals(0, messageManager.getTodayMessagesCount(p2, MessageDirection.RECEIVED));
+		assertEquals(0, messageManager.getTodayMessagesCount(p1, MessageDirection.RECEIVED));
+		assertEquals(0, messageManager.getTodayMessagesCount(p1, MessageDirection.SENT));
+		assertEquals(0, messageManager.getTodayMessagesCount(p2, MessageDirection.SENT));
+		assertEquals(0, messageManager.getTodayMessagesCount(p2, MessageDirection.RECEIVED));
 
-		Assert.assertEquals(0, messageManager.getTodayMessagesCount(p1, MessageDirection.RECEIVED));
-		Assert.assertEquals(0, messageManager.getTodayMessagesCount(p1, MessageDirection.SENT));
-		Assert.assertEquals(0, messageManager.getTodayMessagesCount(p2, MessageDirection.SENT));
-		Assert.assertEquals(0, messageManager.getTodayMessagesCount(p2, MessageDirection.RECEIVED));
+		assertEquals(0, messageManager.getTodayMessagesCount(p1, MessageDirection.RECEIVED));
+		assertEquals(0, messageManager.getTodayMessagesCount(p1, MessageDirection.SENT));
+		assertEquals(0, messageManager.getTodayMessagesCount(p2, MessageDirection.SENT));
+		assertEquals(0, messageManager.getTodayMessagesCount(p2, MessageDirection.RECEIVED));
 
-		Assert.assertEquals(0, messageManager.getNewMessagesCount(p1));
-		Assert.assertEquals(0, messageManager.getNewMessagesCount(p2));
+		assertEquals(0, messageManager.getNewMessagesCount(p1));
+		assertEquals(0, messageManager.getNewMessagesCount(p2));
 
 		messageManager.sendMessage(p1, p2, "B1");
-		Assert.assertEquals(0, messageManager.getNewMessagesCount(p1));
-		Assert.assertEquals(1, messageManager.getNewMessagesCount(p2));
+		assertEquals(0, messageManager.getNewMessagesCount(p1));
+		assertEquals(1, messageManager.getNewMessagesCount(p2));
 
 		messageManager.sendMessage(p1, p2, "B2");
-		Assert.assertEquals(0, messageManager.getNewMessagesCount(p1));
-		Assert.assertEquals(2, messageManager.getNewMessagesCount(p2));
+		assertEquals(0, messageManager.getNewMessagesCount(p1));
+		assertEquals(2, messageManager.getNewMessagesCount(p2));
 
 		messageManager.sendMessage(p2, p1, "B3");
-		Assert.assertEquals(1, messageManager.getNewMessagesCount(p1));
-		Assert.assertEquals(2, messageManager.getNewMessagesCount(p2));
+		assertEquals(1, messageManager.getNewMessagesCount(p1));
+		assertEquals(2, messageManager.getNewMessagesCount(p2));
 
-		Assert.assertEquals(2, messageManager.getMessages(p1, MessageDirection.SENT).size());
-		Assert.assertEquals(1, messageManager.getNewMessagesCount(p1));
-		Assert.assertEquals(2, messageManager.getNewMessagesCount(p2));
+		assertEquals(2, messageManager.getMessages(p1, MessageDirection.SENT).size());
+		assertEquals(1, messageManager.getNewMessagesCount(p1));
+		assertEquals(2, messageManager.getNewMessagesCount(p2));
 
-		Assert.assertEquals(1, messageManager.getMessages(p2, MessageDirection.SENT).size());
-		Assert.assertEquals(1, messageManager.getNewMessagesCount(p1));
-		Assert.assertEquals(2, messageManager.getNewMessagesCount(p2));
+		assertEquals(1, messageManager.getMessages(p2, MessageDirection.SENT).size());
+		assertEquals(1, messageManager.getNewMessagesCount(p1));
+		assertEquals(2, messageManager.getNewMessagesCount(p2));
 
-		Assert.assertEquals(1, messageManager.getMessages(p1, MessageDirection.RECEIVED).size());
-		Assert.assertEquals(1, messageManager.getNewMessagesCount(p1)); // READ_COMMITTED isolation.
-		Assert.assertEquals(2, messageManager.getNewMessagesCount(p2));  // READ_COMMITTED isolation.
+		assertEquals(1, messageManager.getMessages(p1, MessageDirection.RECEIVED).size());
+		assertEquals(1, messageManager.getNewMessagesCount(p1)); // READ_COMMITTED isolation.
+		assertEquals(2, messageManager.getNewMessagesCount(p2));  // READ_COMMITTED isolation.
 
-		Assert.assertEquals(2, messageManager.getMessages(p2, MessageDirection.RECEIVED).size());
-		Assert.assertEquals(1, messageManager.getNewMessagesCount(p1));  // READ_COMMITTED isolation.
-		Assert.assertEquals(2, messageManager.getNewMessagesCount(p2));  // READ_COMMITTED isolation.
+		assertEquals(2, messageManager.getMessages(p2, MessageDirection.RECEIVED).size());
+		assertEquals(1, messageManager.getNewMessagesCount(p1));  // READ_COMMITTED isolation.
+		assertEquals(2, messageManager.getNewMessagesCount(p2));  // READ_COMMITTED isolation.
 
-		Assert.assertEquals(1, messageManager.getTodayMessagesCount(p2, MessageDirection.SENT));
-		Assert.assertEquals(2, messageManager.getTodayMessagesCount(p1, MessageDirection.SENT));
-		Assert.assertEquals(2, messageManager.getTodayMessagesCount(p2, MessageDirection.RECEIVED));
-		Assert.assertEquals(1, messageManager.getTodayMessagesCount(p1, MessageDirection.RECEIVED));
+		assertEquals(1, messageManager.getTodayMessagesCount(p2, MessageDirection.SENT));
+		assertEquals(2, messageManager.getTodayMessagesCount(p1, MessageDirection.SENT));
+		assertEquals(2, messageManager.getTodayMessagesCount(p2, MessageDirection.RECEIVED));
+		assertEquals(1, messageManager.getTodayMessagesCount(p1, MessageDirection.RECEIVED));
 	}
 
 	@Test
 	public void testNotifications() {
-		final Personality p1 = Personality.person(998);
-		final Personality p2 = Personality.person(999);
+		final Player p1 = Guest.byLanguage(Language.RU);
+		final Player p2 = Guest.byLanguage(Language.EN);
 
 		messageManager.sendNotification(p1, "B1");
 		messageManager.sendNotification(p1, "B2");
 		messageManager.sendNotification(p2, "B3");
 
-		Assert.assertEquals(2, messageManager.getMessages(p1, MessageDirection.RECEIVED).size());
-		Assert.assertEquals(1, messageManager.getMessages(p2, MessageDirection.RECEIVED).size());
+		assertEquals(2, messageManager.getMessages(p1, MessageDirection.RECEIVED).size());
+		assertEquals(1, messageManager.getMessages(p2, MessageDirection.RECEIVED).size());
 
 		final Object[] objects = messageManager.getMessages(p1, MessageDirection.RECEIVED).toArray();
 		final Message m1 = (Message) objects[0];
-		Assert.assertEquals(998, m1.getRecipient());
-		Assert.assertEquals("B1", m1.getText());
+		assertEquals(998, m1.getRecipient());
+		assertEquals("B1", m1.getText());
 
 		final Message m2 = (Message) objects[1];
-		Assert.assertEquals(998, m2.getRecipient());
-		Assert.assertEquals("B2", m2.getText());
+		assertEquals(998, m2.getRecipient());
+		assertEquals("B2", m2.getText());
 
 		final Message m3 = (Message) messageManager.getMessages(p2, MessageDirection.RECEIVED).toArray()[0];
-		Assert.assertEquals(999, m3.getRecipient());
-		Assert.assertEquals("B3", m3.getText());
+		assertEquals(999, m3.getRecipient());
+		assertEquals("B3", m3.getText());
 
 		messageManager.removeMessage(p2, m3.getId(), MessageDirection.RECEIVED);
-		Assert.assertEquals(2, messageManager.getMessages(p1, MessageDirection.RECEIVED).size());
-		Assert.assertEquals(0, messageManager.getMessages(p2, MessageDirection.RECEIVED).size());
-		Assert.assertEquals(1, messageManager.getTodayMessagesCount(p2, MessageDirection.RECEIVED));
+		assertEquals(2, messageManager.getMessages(p1, MessageDirection.RECEIVED).size());
+		assertEquals(0, messageManager.getMessages(p2, MessageDirection.RECEIVED).size());
+		assertEquals(1, messageManager.getTodayMessagesCount(p2, MessageDirection.RECEIVED));
 	}
 
 	@Test
@@ -142,7 +158,6 @@ public class HibernateMessageManagerTest {
 		final SessionFactory sessionFactory = EasyMock.createMock(SessionFactory.class);
 		EasyMock.expect(sessionFactory.getCurrentSession()).andReturn(session);
 		EasyMock.replay(sessionFactory);
-*/
 /*
 		expect(template.bulkUpdate("DELETE m FROM player_message as m INNER JOIN account_personality as a ON a.id=m.recipient and " +
                 "((m.notification and " +
@@ -157,7 +172,7 @@ public class HibernateMessageManagerTest {
                 "(a.membership = 'SECOND' and created < DATE_SUB(curdate(), INTERVAL 20 DAY)) or " +
                 "(a.membership = 'FIRST' and created < DATE_SUB(curdate(), INTERVAL 20 DAY)) or " +
                 "(a.membership = 'PLATINUM' and created < DATE_SUB(curdate(), INTERVAL 20 DAY))))")).andReturn(0);
-*//*
+*/
 
 
 		final HibernateMessageManager m = new HibernateMessageManager();
@@ -168,5 +183,4 @@ public class HibernateMessageManagerTest {
 
 		EasyMock.verify(sessionFactory, session, query);
 	}
-*/
 }
