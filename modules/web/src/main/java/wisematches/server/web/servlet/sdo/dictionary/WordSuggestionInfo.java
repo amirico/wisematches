1e29343b141576cf0c66ed5dbdb1ccaf53760db5
smklimenko <smklimenko@localhost>
2013-03-29 17:43:57 +0400

    + Issue 319:	No my own changes

--- modules/web/src/main/java/wisematches/server/web/servlet/sdo/dictionary/WordSuggestionInfo.java
+++ modules/web/src/main/java/wisematches/server/web/servlet/sdo/dictionary/WordSuggestionInfo.java
@@ -1,8 +1,11 @@
 package wisematches.server.web.servlet.sdo.dictionary;
 
+import wisematches.core.Language;
 import wisematches.core.Personality;
 import wisematches.playground.GameMessageSource;
 import wisematches.playground.dictionary.WordAttribute;
+import wisematches.server.services.dictionary.SuggestionState;
+import wisematches.server.services.dictionary.SuggestionType;
 import wisematches.server.services.dictionary.WordSuggestion;
 import wisematches.server.services.state.PlayerStateManager;
 import wisematches.server.web.servlet.sdo.InternationalisedInfo;
@@ -17,60 +20,58 @@ import java.util.Locale;
  * @author Sergey Klimenko (smklimenko@gmail.com)
  */
 public class WordSuggestionInfo extends InternationalisedInfo {
-	private final Personality requestor;
-	private final WordSuggestion suggestion;
-	private final PlayerStateManager stateManager;
+    private final Personality requestor;
+    private final WordSuggestion suggestion;
+    private final PlayerStateManager stateManager;
 
-	public WordSuggestionInfo(Personality requestor, WordSuggestion suggestion, PlayerStateManager stateManager, GameMessageSource messageSource, Locale locale) {
-		super(messageSource, locale);
-		this.requestor = requestor;
-		this.suggestion = suggestion;
-		this.stateManager = stateManager;
-	}
+    public WordSuggestionInfo(Personality requestor, WordSuggestion suggestion, PlayerStateManager stateManager, GameMessageSource messageSource, Locale locale) {
+        super(messageSource, locale);
+        this.requestor = requestor;
+        this.suggestion = suggestion;
+        this.stateManager = stateManager;
+    }
 
-	public String getWord() {
-		return suggestion.getWord();
-	}
+    public String getWord() {
+        return suggestion.getWord();
+    }
 
-	public PersonalityInfo getRequester() {
-		return new PersonalityInfo(messageSource.getPersonalityNick(requestor, locale), requestor, stateManager);
-	}
+    public PersonalityInfo getRequester() {
+        return new PersonalityInfo(messageSource.getPersonalityNick(requestor, locale), requestor, stateManager);
+    }
 
-	public TimeInfo getRequestDate() {
-		return new TimeInfo(suggestion.getRequestDate(), messageSource, locale);
-	}
+    public Language getLanguage() {
+        return suggestion.getLanguage();
+    }
 
-	public String getDefinition() {
-		return suggestion.getDefinition();
-	}
+    public TimeInfo getRequestDate() {
+        return new TimeInfo(suggestion.getRequestDate(), messageSource, locale);
+    }
 
-	public String getCommentary() {
-		return suggestion.getCommentary();
-	}
+    public String getDefinition() {
+        return suggestion.getDefinition();
+    }
 
-	public TimeInfo getResolutionDate() {
-		final Date resolutionDate = suggestion.getResolutionDate();
-		if (resolutionDate != null) {
-			return new TimeInfo(resolutionDate, messageSource, locale);
-		}
-		return null;
-	}
+    public String getCommentary() {
+        return suggestion.getCommentary();
+    }
 
-	public String getSuggestionType() {
-		return messageSource.getMessage("suggestion.type." + suggestion.getSuggestionType().name().toLowerCase() + ".label", locale);
-	}
+    public TimeInfo getResolutionDate() {
+        final Date resolutionDate = suggestion.getResolutionDate();
+        if (resolutionDate != null) {
+            return new TimeInfo(resolutionDate, messageSource, locale);
+        }
+        return null;
+    }
 
-	public String getSuggestionState() {
-		return messageSource.getMessage("suggestion.state." + suggestion.getSuggestionState().name().toLowerCase() + ".label", locale);
-	}
+    public SuggestionType getSuggestionType() {
+        return suggestion.getSuggestionType();
+    }
 
-	public String[] getAttributes() {
-		final EnumSet<WordAttribute> attributes = suggestion.getAttributes();
-		int index = 0;
-		String[] res = new String[attributes.size()];
-		for (WordAttribute attribute : attributes) {
-			res[index++] = messageSource.getMessage("dict.word.attribute." + attribute.name().toLowerCase() + ".label", locale);
-		}
-		return res;
-	}
+    public SuggestionState getSuggestionState() {
+        return suggestion.getSuggestionState();
+    }
+
+    public EnumSet<WordAttribute> getAttributes() {
+        return suggestion.getAttributes();
+    }
 }
