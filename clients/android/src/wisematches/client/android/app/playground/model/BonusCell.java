e20c4fa1d5dd8765d547808113dbed060778dbb4
smklimenko <smklimenko@localhost>
2013-04-11 17:41:44 +0400

    +Issue 124: ads

---
+++ clients/android/src/wisematches/client/android/app/playground/model/BonusCell.java
@@ -0,0 +1,45 @@
+package wisematches.client.android.app.playground.model;
+
+/**
+ * @author Sergey Klimenko (smklimenko@gmail.com)
+ */
+public final class BonusCell {
+	private final int row;
+	private final int column;
+	private final Type type;
+
+	public BonusCell(int row, int column, Type type) {
+		this.row = row;
+		this.column = column;
+		this.type = type;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public static enum Type {
+		L2(0xff9cff00),
+		L3(0xff17e421),
+		W2(0xff00fcff),
+		W3(0xfffffc00);
+
+		private final int color;
+
+		private Type(int color) {
+			this.color = color;
+		}
+
+		public int getColor() {
+			return color;
+		}
+	}
+}
