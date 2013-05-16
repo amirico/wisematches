0efad72bf2cb608ec22e62ed899606b2e4a5f5d7
smklimenko <smklimenko@localhost>
2013-05-16 11:43:22 +0400

    ! jQuery 2.0.0 + JQuery UI 1.10.2 ~ android board view

--- clients/android/src/wisematches/client/android/app/playground/scribble/model/WordDirection.java
+++
@@ -1,9 +0,0 @@
-package wisematches.client.android.app.playground.scribble.model;
-
-/**
- * @author Sergey Klimenko (smklimenko@gmail.com)
- */
-public enum WordDirection {
-	VERTICAL,
-	HORIZONTAL
-}
