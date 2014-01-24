7d01a05f69dacef9ecb12ad95aa89cfa3ae087e9
smklimenko <smklimenko@localhost>
2014-01-24 15:45:34 +0400

    Issue 355:	Indexes & foreighn keys are not correct

--- resources/database/latest_patch.sql
+++ resources/database/latest_patch.sql
@@ -1,13 +1,79 @@
-CREATE TABLE account_UserConnection (userId         VARCHAR(255) NOT NULL,
-                                     providerId     VARCHAR(255) NOT NULL,
-                                     providerUserId VARCHAR(255),
-                                     rank           INT          NOT NULL,
-                                     displayName    VARCHAR(255),
-                                     profileUrl     VARCHAR(512),
-                                     imageUrl       VARCHAR(512),
-                                     accessToken    VARCHAR(255) NOT NULL,
-                                     secret         VARCHAR(255),
-                                     refreshToken   VARCHAR(255),
-                                     expireTime     BIGINT,
-  PRIMARY KEY (userId, providerId, providerUserId));
-CREATE UNIQUE INDEX UserConnectionRank ON account_UserConnection (userId, providerId, rank);
+ALTER TABLE `wisematches`.`account_membership`
+ADD INDEX `TYPE_INDEX` (`membership` ASC);
+
+ALTER TABLE `wisematches`.`award_chronicle`
+ADD INDEX `recipient_idx` (`recipient` ASC),
+ADD INDEX `code_index` (`code` ASC),
+ADD INDEX `weight_index` (`weight` ASC);
+ALTER TABLE `wisematches`.`award_chronicle`
+ADD CONSTRAINT `recipient`
+FOREIGN KEY (`recipient`)
+REFERENCES `wisematches`.`account_personality` (`id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `wisematches`.`dictionary_reclaim`
+ADD CONSTRAINT `requestor`
+FOREIGN KEY (`requester`)
+REFERENCES `wisematches`.`account_personality` (`id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `wisematches`.`player_message`
+ADD INDEX `rec_idx` (`recipient` ASC);
+ALTER TABLE `wisematches`.`player_message`
+ADD CONSTRAINT `rec`
+FOREIGN KEY (`recipient`)
+REFERENCES `wisematches`.`account_personality` (`id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `wisematches`.`player_message`
+ADD INDEX `state_index` (`state` ASC),
+ADD INDEX `original_index` (`original` ASC),
+ADD INDEX `sender_index` (`sender` ASC);
+
+ALTER TABLE `wisematches`.`scribble_board`
+ADD INDEX `currentPlayer_index` (`currentPlayerIndex` ASC),
+ADD INDEX `resolution_index` (`resolution` ASC),
+ADD INDEX `language_index` (`language` ASC),
+ADD INDEX `days_index` (`daysPerMove` ASC),
+ADD INDEX `last_move_index` (`lastMoveTime` ASC);
+
+ALTER TABLE `wisematches`.`scribble_comment`
+ADD INDEX `person_idx` (`person` ASC),
+ADD INDEX `read_index` (`read` ASC),
+ADD INDEX `board_idx` (`board` ASC);
+ALTER TABLE `wisematches`.`scribble_comment`
+ADD CONSTRAINT `person`
+FOREIGN KEY (`person`)
+REFERENCES `wisematches`.`account_personality` (`id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `board`
+FOREIGN KEY (`board`)
+REFERENCES `wisematches`.`scribble_board` (`boardId`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `wisematches`.`scribble_memory`
+ADD INDEX `person_idx` (`playerId` ASC);
+ALTER TABLE `wisematches`.`scribble_memory`
+ADD CONSTRAINT `person`
+FOREIGN KEY (`playerId`)
+REFERENCES `wisematches`.`account_personality` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `wisematches`.`scribble_statistic`
+ADD INDEX `rating` (`rating` ASC);
+
+ALTER TABLE `wisematches`.`tourney_regular_division`
+ADD CONSTRAINT `tourney`
+FOREIGN KEY (`tourneyId`)
+REFERENCES `wisematches`.`tourney_regular` (`id`)
+  ON DELETE CASCADE
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `wisematches`.`tourney_regular_division`
+ADD INDEX `language_index` (`language` ASC);
